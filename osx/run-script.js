31e1dad853416a14d66458b59660afab23c33ef2
Dan Hromada <hromada.dan@gmail.com>
2019-11-14 09:21:45 +0100

    Scope script runner

--- osx/run-script.js
+++ osx/run-script.js
@@ -1,13 +1,15 @@
 // RUN SCRIPT
-const callback = () => {
-  if (!window.main) {
-    console.error('Bundle not injected');
-    return;
-  }
-  safari.extension.dispatchMessage('Hlídač Shopů');
-  main().catch(err => console.error(err));
-  console.log('👋 Safari');
-};
+(global => {
+  const callback = () => {
+    if (!global.main) {
+      console.error('Bundle not injected');
+      return;
+    }
+    safari.extension.dispatchMessage('Hlídač Shopů');
+    main().catch(err => console.error(err));
+    console.log('👋 Safari');
+  };
 
-document.addEventListener('DOMContentLoaded', callback);
-if (document.readyState !== 'loading') callback();
+  document.addEventListener('DOMContentLoaded', callback);
+  if (document.readyState !== 'loading') callback();
+})(window);
