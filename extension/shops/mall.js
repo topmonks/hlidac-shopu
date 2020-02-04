97696c87e7658d2a5e2e4c18f20b7663c9f9e7d2
Palciny <palciny@gmail.com>
2020-02-04 13:46:12 +0100

    Update mall.js
    Adjustments in js after mall.cz has changed design.

--- extension/shops/mall.js
+++ extension/shops/mall.js
@@ -7,7 +7,7 @@ window.shops["mall"] = {
   },
 
   getInfo() {
-    const elem = document.querySelector(".price-wrapper");
+    const elem = document.querySelector(".prices-wrapper");
     if (!elem) return;
 
     const itemId = document
@@ -17,12 +17,12 @@ window.shops["mall"] = {
       .querySelector('h1[itemprop="name"]')
       .innerText.trim();
     const currentPrice = cleanPrice("[itemprop=price]");
-    const originalPrice = cleanPrice(".old-new-price .rrp-price");
+    const originalPrice = cleanPrice(".old-price > del:nth-child(1)");
     return { itemId, title, currentPrice, originalPrice };
   },
 
   insertChartElement(chartMarkup) {
-    const elem = document.querySelector(".product-footer");
+    const elem = document.querySelector(".other-options-box");
     if (!elem) throw new Error("Element to add chart not found");
 
     const markup = chartMarkup();
