d686c9d89013417248710379e0898b0f69a28fd1
Ondřej <ondrej.svoboda@topmonks.com>
2020-07-13 12:47:16 +0200

    fix mironet orig price

--- extension/shops/mironet.js
+++ extension/shops/mironet.js
@@ -13,7 +13,7 @@ window.shops["mironet"] = {
       ".product_kosik_info input[name=Code]"
     ).value;
     const title = document.querySelector("h1").textContent.trim();
-    const currentPrice = cleanPrice(".product_cena_box .product_dph");
+    const currentPrice = cleanPrice(".product_cena_box .product_dph span");
     const originalPrice = cleanPrice(".fakcbox23 .product_dph");
 
     return { itemId, title, currentPrice, originalPrice };
