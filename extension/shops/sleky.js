18828f5b362f1674b2fc86a9d205bb248a30ff27
Ondřej <ondrej.svoboda@topmonks.com>
2020-07-15 14:10:01 +0200

    Add sleky

---
+++ extension/shops/sleky.js
@@ -0,0 +1,47 @@
+/* global cleanPrice */
+
+window.shops = window.shops || {};
+window.shops["sleky"] = {
+  onDetailPage(cb) {
+    cb();
+  },
+
+  waitForElement(selector, timeout = 5) {
+    return new Promise((resolve, reject) => {
+      let count = 0;
+      const getElement = () => {
+        count++;
+        const elem = document.querySelector(selector);
+        if (elem) {
+          return resolve(elem);
+        }
+        if (count >= timeout) {
+          return reject();
+        }
+        setTimeout(getElement, 100);
+      };
+      getElement();
+    });
+  },
+
+  async getInfo() {
+    const title = document.querySelector("h1[itemprop='name']").innerText;
+    const form = document.querySelector(".orderbox form");
+    const itemId = form.dataset.productId;
+    const currentPrice = cleanPrice(form.querySelector("strong.fullprice"));
+    const originalPrice = cleanPrice(form.querySelector("dl>dt+dd"));
+
+    return { itemId, title, currentPrice, originalPrice, dataType: "dynamo" };
+  },
+
+  insertChartElement(chartMarkup) {
+    const elem = document.querySelector(".orderbox");
+    // const elem = document.querySelector(".desc>hr");
+    if (!elem) throw new Error("Element to add chart not found");
+
+    const markup = chartMarkup();
+    elem.insertAdjacentHTML("afterend", markup);
+    return elem;
+  }
+};
+
