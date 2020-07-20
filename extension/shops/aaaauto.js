45c24cfd64d2f9451dc54d7e3d0ce3590405ebf1
Ondřej <ondrej.svoboda@topmonks.com>
2020-07-20 16:47:15 +0200

    Fix aaaauto eng variant

--- extension/shops/aaaauto.js
+++ extension/shops/aaaauto.js
@@ -9,7 +9,23 @@ window.shops["aaaauto"] = {
   getInfo() {
     const url = new URL(location.href);
     const itemId = url.searchParams.get("id");
+    if (!itemId) return false;
 
+    // eng variant
+    const engTabCard = document.querySelector("#tab-card");
+    if (engTabCard) {
+      const title = engTabCard.querySelector("h1").textContent;
+      const priceRows = engTabCard.querySelectorAll("#priceTable .priceRow");
+      let currentPrice;
+      if (priceRows.length == 2) {
+        currentPrice = cleanPrice(engTabCard.querySelector("#priceTable .carPrice span"));
+      } else {
+        currentPrice = cleanPrice(engTabCard.querySelector("#priceTable .priceRow:last-child span"));
+      }
+
+      const originalPrice = null;
+      return { itemId, title, currentPrice, originalPrice, dataType: "dynamo" };
+    }
     const title = document.querySelector("#carCardHead h1").innerText;
     const price = document.querySelector(".sidebar ul.infoBoxNav li:not([style]):not([class]) span.notranslate");
     const originalPrice = cleanPrice(price.firstChild);
@@ -19,9 +35,19 @@ window.shops["aaaauto"] = {
   },
 
   insertChartElement(chartMarkup) {
-    const elem = document.querySelector(".sidebar .infoBox .btnSubText");
+    let elem = document.querySelector(".sidebar .infoBox .btnSubText");
+    if (elem) {
+      const markup = chartMarkup();
+      elem.insertAdjacentHTML("afterend", markup);
+      return elem;
+    }
+
+    // eng variant
+    elem = document.querySelector("#carButtons .testdrive-bonus");
     if (!elem) throw new Error("Element to add chart not found");
 
+    const table = document.querySelector("#carButtons table");
+    table.style.position = "relative";
     const markup = chartMarkup();
     elem.insertAdjacentHTML("afterend", markup);
     return elem;
