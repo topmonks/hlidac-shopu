5b342e526938c55a8a952e87501cdfe2803e247c
Aleš Roubíček <ales@roubicek.name>
2020-10-15 14:25:59 +0200

    Provision infrastructure on master, preview on other branches

--- index.ts
+++ index.ts
@@ -4,6 +4,8 @@ import * as awsx from "@pulumi/awsx";
 import {
   Website,
   createCertificate,
+  createTxtRecord,
+  createGoogleMxRecords,
   registerAutoTags,
   SecurityHeadersLambda,
   AssetsCachingLambda
@@ -15,13 +17,17 @@ registerAutoTags({
 });
 
 let certificate = createCertificate("www.hlidacshopu.cz");
-let securityHeadersLambda = SecurityHeadersLambda.create("hlidac-shopu-security");
+let securityHeadersLambda = SecurityHeadersLambda.create(
+  "hlidac-shopu-security"
+);
 let assetsCachingLambda = AssetsCachingLambda.create("hlidac-shopu-caching");
+let googleVerification = createTxtRecord(
+  "google-verification",
+  "hlidacshopu.cz",
+  "google-site-verification=95Q6P8PAOMniBIXaI_FgFgE4iPPjknOH8lNH9lJ88bA"
+);
+let gmailRecords = createGoogleMxRecords("hlidacshopu.cz");
 
-let website = Website.create("www.hlidacshopu.cz", {
-  assetsCachingLambdaArn: assetsCachingLambda.arn,
-  securityHeadersLambdaArn: securityHeadersLambda.arn
-});
 let nakedDomainRedirect = Website.createRedirect("hlidacshopu.cz", {
   target: "https://www.hlidacshopu.cz"
 });
@@ -29,9 +35,5 @@ let nakedDomainRedirect = Website.createRedirect("hlidacshopu.cz", {
 export const certificateArn = certificate;
 export const assetsCachingLambdaArn = assetsCachingLambda.arn;
 export const securityHeadersLambdaArn = securityHeadersLambda.arn;
-export const websiteUrl = website.url;
-export const websiteS3BucketUri = website.s3BucketUri;
-export const websiteS3WebsiteUrl = website.s3WebsiteUrl;
-export const websiteCloudFrontId = website.cloudFrontId;
 export const nakedDomainRedirectUrl = nakedDomainRedirect.url;
 export const nakedDomainRedirectCloudFrontId = nakedDomainRedirect.cloudFrontId;
