1a3d8681b06342a7ef6fd204e1a122e9253ef67f
Aleš Roubíček <ales@roubicek.name>
2020-10-30 16:09:42 +0100

    Remove results implementation from HP and use embedded PWA instead

--- www.hlidacshopu.cz/src/javascripts/index.js
+++ www.hlidacshopu.cz/src/javascripts/index.js
@@ -1,12 +1,6 @@
 import { html, svg, render } from "lit-html/lit-html.js";
 import { shops } from "./lib/shops.js";
-import { formatPercents, formatMoney } from "./lib/format.js";
-import {
-  initChart,
-  fetchDownloadStats,
-  fetchShopsStats,
-  templateData
-} from "./lib/remoting";
+import { fetchDownloadStats, fetchShopsStats } from "./lib/remoting";
 
 const form = document.getElementById("compare-form");
 const eShops = document.getElementById("e-shopy");
@@ -18,7 +12,6 @@ const modalRenderRoot = document.getElementById(
   "hlidac-shopu-modal__placeholder"
 );
 const installationGuide = document.getElementById("extension-install-guide");
-const chart = () => document.getElementById("hlidac-shopu-chart");
 
 const haveToCloseModal = t =>
   t === modal ||
@@ -105,16 +98,8 @@ function hideResultsModal() {
 }
 
 async function renderResultsModal(detailUrl) {
-  render(loaderTemplate(), modalRenderRoot);
+  render(resultsEmbedd(detailUrl), modalRenderRoot);
   showResultsModal();
-  try {
-    const [{ plot }, chartData] = await initChart(detailUrl);
-    render(resultTemplate(templateData(detailUrl, chartData)), modalRenderRoot);
-    plot(chart(), chartData);
-  } catch (ex) {
-    console.error(ex);
-    render(notFoundTemplate(), modalRenderRoot);
-  }
 }
 
 // explicit map of URLs for guides, to be rev-updated in production build
@@ -136,123 +121,9 @@ function getInstallationGuideUrl(searchParams) {
   return guides.get(browser);
 }
 
-function notFoundTemplate() {
-  return html`
-    <div
-      id="hlidac-shopu-modal__not-found"
-      class="result mdc-layout-grid__inner"
-    >
-      <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-12">
-        <h2>Nenalezeno</h2>
-      </div>
-      <div
-        class="mdc-layout-grid__cell mdc-layout-grid__cell--span-12 box box--purple"
-      >
-        <p>
-          Je nám líto, ale hledaný produkt nebo e-shop nemáme v naší databázi.
-        </p>
-      </div>
-    </div>
-  `;
-}
-
-function loaderTemplate() {
-  return html`
-    <div id="hlidac-shopu-modal__loader" class="result mdc-layout-grid__inner">
-      <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-12">
-        <h2>Ověřuji&hellip;</h2>
-      </div>
-      <div
-        class="mdc-layout-grid__cell mdc-layout-grid__cell--span-12 box box--purple"
-      >
-        <div class="loading-container">
-          <div class="loader" aria-label="Načítám data…">
-            Váš požadavek se zpracovává&hellip;
-          </div>
-        </div>
-      </div>
-    </div>
-  `;
-}
-
-function resultTemplate({
-  detailUrl,
-  name,
-  shop,
-  lastDeclaredPrice,
-  actualPrice,
-  discount,
-  date
-}) {
-  const declaredOriginalPrice = x =>
-    x &&
-    html`
-      <div class="claimed-price">
-        Uváděná původní cena <del id="original-price">${formatMoney(x)}</del>
-      </div>
-    `;
-  const realDiscount = x =>
-    x !== null &&
-    !isNaN(x) &&
-    html`
-      <div>
-        <abbr
-          title="Reálná sleva se počítá podle EU směrnice jako aktuální cena po slevě ku minimální ceně, za kterou se zboží prodávalo v období 30 dní před slevovou akcí."
-          >Reálná sleva*</abbr
-        >
-        <b class="discount"
-          ><span id="real-discount">${formatPercents(x)}</span></b
-        >
-      </div>
-    `;
-  const crawlDate = x =>
-    x &&
-    html`
-      <time id="latest-date" datetime="${x.toISOString()}">
-        ${x.toLocaleString("cs", {
-          day: "numeric",
-          month: "long",
-          year: "numeric"
-        })}
-      </time>
-    `;
-  const shopLogo = x => x && logoTemplate(x);
-  return html`
-    <div id="hlidac-shopu-modal__found" class="result mdc-layout-grid__inner">
-      <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-12">
-        <h2>
-          ${shopLogo(shops.get(shop))}<a href="${detailUrl}" id="product-name"
-            >${name || "Vámi vybraný produkt"}</a
-          >
-        </h2>
-      </div>
-      <div
-        class="mdc-layout-grid__cell mdc-layout-grid__cell--span-12 box box--purple"
-      >
-        ${crawlDate(date)} ${declaredOriginalPrice(lastDeclaredPrice)}
-        <div class="actual-price">
-          Prodejní cena
-          <span id="current-price">${formatMoney(actualPrice)}</span>
-        </div>
-        ${realDiscount(discount) || ""}
-      </div>
-      <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-12">
-        <canvas id="hlidac-shopu-chart" width="100%"></canvas>
-      </div>
-      <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-12">
-        <div style="display:flex;justify-content: flex-end">
-          <div
-            style="width:12px;height:12px;background-color:#5c62cd;border-radius:2px;margin-right:5px;margin-top:2px;"
-          ></div>
-          <span>Uváděná původní cena</span>
-          <div
-            style="width:12px;height:12px;background-color:#ff8787;border-radius:2px;margin: 2px 5px 0 8px;"
-          ></div>
-          <span>Prodejní cena</span>
-        </div>
-      </div>
-    </div>
-  `;
+function resultsEmbedd(url) {
+  const parameters = new URLSearchParams({ url }).toString();
+  return html`<iframe class="hs-result__embedd" src="/app/?${parameters}"></iframe>`;
 }
 
 function logoTemplate({ logo, name, url, viewBox }) {
