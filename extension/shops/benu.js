553434e032fc82b39e90d25249bfdb43c0f72faf
Aleš Roubíček <ales@roubicek.name>
2020-11-05 16:15:51 +0100

    Fixed Benu

--- extension/shops/benu.js
+++ extension/shops/benu.js
@@ -6,18 +6,21 @@ const benu = {
   },
 
   getInfo() {
-    const title = document.querySelector(".product-title-rating .title")
+    const richSnippet = JSON.parse(document.querySelector("#snippet-productRichSnippet-richSnippet").innerText);
+
+    const title = richSnippet.name || document.querySelector(".product-title-rating .title")
       .innerText;
+    const itemId = richSnippet.identifier;
     const currentPrice = cleanPrice(".buy strong.buy-box__big-price");
     const originalPrice = cleanPrice(".buy .buy-box__price-head del");
     const imageUrl = document.querySelector("meta[property='og:image']")
       .content;
 
-    return { title, currentPrice, originalPrice, imageUrl };
+    return { title, itemId, currentPrice, originalPrice, imageUrl };
   },
 
   insertChartElement(chartMarkup) {
-    const elem = document.querySelector(".buy-box__price-foot");
+    const elem = document.querySelector(".buy-box");
     if (!elem) throw new Error("Element to add chart not found");
 
     const markup = chartMarkup();
