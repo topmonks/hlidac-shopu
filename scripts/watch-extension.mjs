3d92a1e17b598ec59c8b32bc5219cf91123d14c5
Aleš Roubíček <ales@roubicek.name>
2020-11-09 14:13:22 +0100

    Build watcher for extension

---
+++ scripts/watch-extension.mjs
@@ -0,0 +1,28 @@
+import chokidar from "chokidar";
+import esbuild from "esbuild";
+import path from "path";
+
+async function build() {
+  const service = await esbuild.startService();
+  try {
+    console.time("build");
+    await service.build({
+      color: true,
+      entryPoints: [entryPoint],
+      outfile: output,
+      bundle: true
+    });
+    console.timeEnd("build");
+  } catch (err) {
+    console.error(err);
+  } finally {
+    service.stop();
+  }
+}
+
+console.log("Watchin extension files");
+
+build();
+
+const watcher = chokidar.watch(["lib/**/*.js", "extension/**/*.mjs"]);
+watcher.on("change", () => build());
