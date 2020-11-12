67d89d31b1526fcc81fb176816d636e25b314eeb
Dan Hromada <hromada.dan@gmail.com>
2020-11-12 17:05:23 +0100

    Fresh Safari 14 extension wrapper

---
+++ apple/osx/hlidac shopu/hlidac shopu/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  hlidac shopu
+//
+//  Created by Daniel Hromada on 11/11/2020.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+}
