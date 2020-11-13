178787db0ff3080d19655360eeb0a9e48bc5eb01
Dan Hromada <hromada.dan@gmail.com>
2020-11-13 14:23:24 +0100

    Use xcrun agvtool to set correct version and bundle number

--- scripts/set-osx-version.js
+++ scripts/set-osx-version.js
@@ -1,30 +1,17 @@
 #!/usr/bin/env node
 const path = require('path');
+const { execSync } = require("child_process");
 
 const __ext_path = path.resolve(__dirname, "../extension");
 const __osx_path = path.resolve(__dirname, "../apple/osx/hlidac shopu");
 const __osx_name = "hlidac shopu";
 
 try {
-  console.log(`Updation Mac app version`);
-  const info_plist_path = `${__osx_path}/${__osx_name}/Info.plist`;
-
-  const fs = require("fs");
-  const plist = require("plist");
-  const info_plist = plist.parse(fs.readFileSync(info_plist_path, "utf8"));
-
   const { version } = require(`${__ext_path}/manifest.json`);
-  const bundleNumber = info_plist['CFBundleVersion'] + 1;
+  execSync(`pushd "${__osx_path}"; xcrun agvtool new-marketing-version ${version}; popd;`);
+  execSync(`pushd "${__osx_path}"; xcrun agvtool next-version -all; popd;`); // this jus increments build num
 
-
-  info_plist["CFBundleShortVersionString"] = version;
-  info_plist["CFBundleVersion"] = bundleNumber;
-
-  fs.writeFileSync(info_plist_path, plist.build(info_plist), {
-    encoding: "utf8",
-    flag: "w"
-  });
-  console.log(`Mac app version is now ${version} (${bundleNumber})`);
+  console.log(`Mac app version is now ${version}`);
 } catch (error) {
   console.error("Failed to set Mac app version", error);
 }
