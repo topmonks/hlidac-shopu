831f0a6823f5424c7fe15a932e2eddde7cac91ba
Aleš Roubíček <ales@roubicek.name>
2020-12-05 03:52:20 +0100

    Updated workbox

--- www.hlidacshopu.cz/src/javascripts/sw.js
+++ www.hlidacshopu.cz/src/javascripts/sw.js
@@ -1,5 +1,5 @@
 importScripts(
-  "https://storage.googleapis.com/workbox-cdn/releases/6.0.0/workbox-sw.js"
+  "https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js"
 );
 
 addEventListener("message", event => {
