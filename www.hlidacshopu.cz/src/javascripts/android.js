47272fc2ee5b0e144b83ea073320c22ab7720fa4
Aleš Roubíček <ales@roubicek.name>
2020-12-09 07:07:24 +0100

    Android guide

--- www.hlidacshopu.cz/src/javascripts/android.js
+++ www.hlidacshopu.cz/src/javascripts/android.js
@@ -2,15 +2,145 @@ import { html } from "lit-html/lit-html.js";
 
 export function installationGuide() {
   return html`
-    <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-12">
+    <div class="steps mdc-layout-grid__cell mdc-layout-grid__cell--span-12">
       <h3>Hlídač shopů pro mobilní zařízení Android</h3>
-      <p>
-        Rozšíření Hlídač shopů funguje zatím pouze ve
-        <a
-          href="https://play.google.com/store/apps/details?id=org.mozilla.firefox&amp;hl=cs"
-          >Firefoxu pro Android</a
-        >.
+    </div>
+    <div
+      class="step_1 mdc-layout-grid__cell mdc-layout-grid__cell--span-8-tablet"
+    >
+      <picture>
+        <source
+          media="screen and (max-width: 479px)"
+          srcset="
+            https://res.cloudinary.com/topmonks/image/upload/dpr_1,q_auto,f_auto,w_380/v1607452729/www.hlidacshopu.cz/search-android-2.png   1x,
+            https://res.cloudinary.com/topmonks/image/upload/dpr_1.5,q_auto,f_auto,w_380/v1607452729/www.hlidacshopu.cz/search-android-2.png 1.5x,
+            https://res.cloudinary.com/topmonks/image/upload/dpr_2,q_auto,f_auto,w_380/v1607452729/www.hlidacshopu.cz/search-android-2.png   2x
+          "
+        />
+        <source
+          media="screen and (min-width: 480px) and (max-width: 839px)"
+          srcset="
+            https://res.cloudinary.com/topmonks/image/upload/dpr_1,q_auto,f_auto,w_740/v1607452729/www.hlidacshopu.cz/search-android-2.png   1x,
+            https://res.cloudinary.com/topmonks/image/upload/dpr_1.5,q_auto,f_auto,w_740/v1607452729/www.hlidacshopu.cz/search-android-2.png 1.5x,
+            https://res.cloudinary.com/topmonks/image/upload/dpr_2,q_auto,f_auto,w_740/v1607452729/www.hlidacshopu.cz/search-android-2.png   2x
+          "
+        />
+        <source
+          media="screen and (min-width: 840px)"
+          srcset="
+            https://res.cloudinary.com/topmonks/image/upload/dpr_1,q_auto,f_auto,w_340/v1607452729/www.hlidacshopu.cz/search-android-2.png   1x,
+            https://res.cloudinary.com/topmonks/image/upload/dpr_1.5,q_auto,f_auto,w_340/v1607452729/www.hlidacshopu.cz/search-android-2.png 1.5x,
+            https://res.cloudinary.com/topmonks/image/upload/dpr_2,q_auto,f_auto,w_340/v1607452729/www.hlidacshopu.cz/search-android-2.png   2x
+          "
+        />
+        <img
+          loading="lazy"
+          src="https://res.cloudinary.com/topmonks/image/upload/dpr_auto,q_auto,f_auto,w_auto/v1607452729/www.hlidacshopu.cz/search-android-2.png"
+          alt="Přejít na PWA - Obrázkový Průvodce"
+          width="100%"
+        />
+      </picture>
+      <h3>Otevřete si naše PWA</h3>
+      <p class="grey-text">
+        Přejděte do
+        <a href="/app/" class="text--purple">Hlídače Shopů do mobilu</a>
+        a klikněte na instalační baner.
       </p>
+    </div>
+    <div
+      class="step_2 mdc-layout-grid__cell mdc-layout-grid__cell--span-8-tablet"
+    >
+      <picture>
+        <source
+          media="screen and (max-width: 479px)"
+          srcset="
+            https://res.cloudinary.com/topmonks/image/upload/dpr_1,q_auto,f_auto,w_380/v1607452746/www.hlidacshopu.cz/install-android-2.png   1x,
+            https://res.cloudinary.com/topmonks/image/upload/dpr_1.5,q_auto,f_auto,w_380/v1607452746/www.hlidacshopu.cz/install-android-2.png 1.5x,
+            https://res.cloudinary.com/topmonks/image/upload/dpr_2,q_auto,f_auto,w_380/v1607452746/www.hlidacshopu.cz/install-android-2.png   2x
+          "
+        />
+        <source
+          media="screen and (min-width: 480px) and (max-width: 839px)"
+          srcset="
+            https://res.cloudinary.com/topmonks/image/upload/dpr_1,q_auto,f_auto,w_740/v1607452746/www.hlidacshopu.cz/install-android-2.png   1x,
+            https://res.cloudinary.com/topmonks/image/upload/dpr_1.5,q_auto,f_auto,w_740/v1607452746/www.hlidacshopu.cz/install-android-2.png 1.5x,
+            https://res.cloudinary.com/topmonks/image/upload/dpr_2,q_auto,f_auto,w_740/v1607452746/www.hlidacshopu.cz/install-android-2.png   2x
+          "
+        />
+        <source
+          media="screen and (min-width: 840px)"
+          srcset="
+            https://res.cloudinary.com/topmonks/image/upload/dpr_1,q_auto,f_auto,w_340/v1607452746/www.hlidacshopu.cz/install-android-2.png   1x,
+            https://res.cloudinary.com/topmonks/image/upload/dpr_1.5,q_auto,f_auto,w_340/v1607452746/www.hlidacshopu.cz/install-android-2.png 1.5x,
+            https://res.cloudinary.com/topmonks/image/upload/dpr_2,q_auto,f_auto,w_340/v1607452746/www.hlidacshopu.cz/install-android-2.png   2x
+          "
+        />
+        <img
+          loading="lazy"
+          src="https://res.cloudinary.com/topmonks/image/upload/dpr_auto,q_auto,f_auto,w_auto/v1607452746/www.hlidacshopu.cz/install-android-2.png"
+          alt="Instalovat Rozšíření - Obrázkový Průvodce"
+          width="100%"
+        />
+      </picture>
+      <h3>Přidejte si Hlídače Shopů do mobilu</h3>
+      <p class="grey-text">
+        Potvrďte přidání Hlídače Shopů na domovskou stránku vašeho telefonu
+      </p>
+    </div>
+    <div
+      class="step_3 mdc-layout-grid__cell mdc-layout-grid__cell--span-8-tablet"
+    >
+      <picture>
+        <source
+          media="screen and (max-width: 479px)"
+          srcset="
+            https://res.cloudinary.com/topmonks/image/upload/dpr_1,q_auto,f_auto,w_380/v1607493835/www.hlidacshopu.cz/usage-android-2.png   1x,
+            https://res.cloudinary.com/topmonks/image/upload/dpr_1.5,q_auto,f_auto,w_380/v1607493835/www.hlidacshopu.cz/usage-android-2.png 1.5x,
+            https://res.cloudinary.com/topmonks/image/upload/dpr_2,q_auto,f_auto,w_380/v1607493835/www.hlidacshopu.cz/usage-android-2.png   2x
+          "
+        />
+        <source
+          media="screen and (min-width: 480px) and (max-width: 839px)"
+          srcset="
+            https://res.cloudinary.com/topmonks/image/upload/dpr_1,q_auto,f_auto,w_740/v1607493835/www.hlidacshopu.cz/usage-android-2.png   1x,
+            https://res.cloudinary.com/topmonks/image/upload/dpr_1.5,q_auto,f_auto,w_740/v1607493835/www.hlidacshopu.cz/usage-android-2.png 1.5x,
+            https://res.cloudinary.com/topmonks/image/upload/dpr_2,q_auto,f_auto,w_740/v1607493835/www.hlidacshopu.cz/usage-android-2.png   2x
+          "
+        />
+        <source
+          media="screen and (min-width: 840px)"
+          srcset="
+            https://res.cloudinary.com/topmonks/image/upload/dpr_1,q_auto,f_auto,w_340/v1607493835/www.hlidacshopu.cz/usage-android-2.png   1x,
+            https://res.cloudinary.com/topmonks/image/upload/dpr_1.5,q_auto,f_auto,w_340/v1607493835/www.hlidacshopu.cz/usage-android-2.png 1.5x,
+            https://res.cloudinary.com/topmonks/image/upload/dpr_2,q_auto,f_auto,w_340/v1607493835/www.hlidacshopu.cz/usage-android-2.png   2x
+          "
+        />
+        <img
+          loading="lazy"
+          src="https://res.cloudinary.com/topmonks/image/upload/dpr_auto,q_auto,f_auto,w_auto/v1607493835/www.hlidacshopu.cz/usage-android-2.png"
+          alt="Použití Rozšíření - Obrázkový Průvodce"
+          width="100%"
+        />
+      </picture>
+      <h3>Využijte systémového sdílení</h3>
+      <p class="grey-text">
+        Otevřete si požadovaný produkt v prohlížeči nebo mobilní aplikaci a
+        pomocí sdílení ho nechte <b>Ověřit v Hlídači Shopů</b>
+      </p>
+    </div>
+    <div
+      class="final-step mdc-layout-grid__cell mdc-layout-grid__cell--span-12"
+    >
+      <p>
+        <b>A&nbsp;máte hotovo! Skvěle.</b><br />
+        Teď už jen stačí přejít do vašeho oblíbeného e-shopu a vesele
+        kontrolovat jeho cenotvorbu.
+      </p>
+      <div class="btn">
+        <a data-browser="pwa" class="button" role="button" href="/app/"
+          >Nainstalovat do mobilu</a
+        >
+      </div>
     </div>
   `;
 }
