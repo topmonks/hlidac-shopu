29f109fdc2e6646827f5b90eafef4a1d39070893
Aleš Roubíček <ales@roubicek.name>
2020-12-21 11:54:31 +0100

    Results refactoring

--- www.hlidacshopu.cz/src/javascripts/app.js
+++ www.hlidacshopu.cz/src/javascripts/app.js
@@ -2,17 +2,9 @@ import { html, render } from "lit-html/lit-html.js";
 import { MDCTopAppBar } from "@material/top-app-bar/component.js";
 import { MDCSnackbar } from "@material/snackbar/component.js";
 import { Workbox } from "workbox-window/build/workbox-window.prod.mjs";
-import { shopName, shops } from "@hlidac-shopu/lib/shops.mjs";
-import { formatDate, formatMoney } from "@hlidac-shopu/lib/format.mjs";
-import { fetchDataSet, templateData } from "@hlidac-shopu/lib/remoting.mjs";
-import "@hlidac-shopu/lib/web-components/chart.mjs";
-import {
-  discountTemplate,
-  loaderTemplate,
-  logoTemplate,
-  notFoundTemplate,
-  originalPriceTemplate
-} from "@hlidac-shopu/lib/templates.mjs";
+import { shopName } from "@hlidac-shopu/lib/shops.mjs";
+import { loaderTemplate } from "@hlidac-shopu/lib/templates.mjs";
+import { fetchDataSet } from "@hlidac-shopu/lib/remoting.mjs";
 import * as rollbar from "./rollbar.js";
 
 rollbar.init();
@@ -41,26 +33,21 @@ addEventListener("DOMContentLoaded", async () => {
     const sharedInfo = getSharedInfo(url);
     console.log("Shared data:", sharedInfo);
     if (sharedInfo) {
-      root.parentElement.classList.remove("home-screen");
-      root.parentElement.classList.add("result-screen");
-      await renderResultsModal(
-        sharedInfo.targetURL,
-        sharedInfo.view === "embed"
-      );
+      await renderResults(root, sharedInfo);
     } else if (url.searchParams.has("install")) {
-      installBanner.classList.add("hs-install-banner--visible");
+      showInstallBanner();
     } else if (url.searchParams.has("help")) {
-      help.classList.add("help--visible");
+      showHelp();
     }
     if (!navigator.share) {
-      shareButton.style.display = "none";
+      hideShareButton();
     }
     if (!navigator.onLine) {
-      progressBar.classList.remove("hs-progress-bar--online");
+      showOfflineBanner();
     }
     if (isWebView(navigator.userAgent)) {
       logoLink.href = "/app/";
-      if (help) help.classList.add("help--visible");
+      if (help) showHelp();
     }
     performance.mark("UI ready");
   } catch (err) {
@@ -76,27 +63,23 @@ addEventListener("DOMContentLoaded", async () => {
 });
 
 addEventListener("offline", () => {
-  progressBar.classList.remove("hs-progress-bar--online");
-  progressBar.classList.add("mdc-top-app-bar--fixed-adjust");
-  const form = document.querySelector(".hs-form");
-  if (form) form.firstElementChild.setAttribute("disabled", "disabled");
+  showOfflineBanner();
+  disableForm();
 });
 
 addEventListener("online", () => {
-  progressBar.classList.add("hs-progress-bar--online");
-  progressBar.classList.remove("mdc-top-app-bar--fixed-adjust");
-  const form = document.querySelector(".hs-form");
-  if (form) form.firstElementChild.removeAttribute("disabled");
+  hideOfflineBanner();
+  enableForm();
 });
 
 addEventListener("beforeinstallprompt", e => {
   e.preventDefault();
   const installPrompt = e;
   if (!installBanner) return;
-  installBanner.classList.add("hs-install-banner--visible");
+  showInstallBanner();
   installBanner.addEventListener("click", () => {
     installPrompt.prompt();
-    installBanner.classList.remove("hs-install-banner--visible");
+    hideInstallBanner();
   });
 });
 
@@ -117,14 +100,6 @@ searchButton.addEventListener("click", () => {
 
 function isWebView(ua) {
   return /(iPhone|iPod|iPad).*AppleWebKit(?!.*Safari)/i.test(ua);
-}
-
-function showUpdateSnackbar() {
-  snackbarElement.style.display = "block";
-  snackbar.actionButtonText = "Aktualizovat";
-  snackbar.labelText = "K dispozici je nová verze.";
-  snackbar.timeoutMs = -1;
-  snackbar.open();
 }
 
 function registerUpdateHandler(wb) {
@@ -173,22 +148,6 @@ function getSharedInfo({ searchParams }) {
   return targetURL && { title, targetURL, shop, view };
 }
 
-async function renderResultsModal(detailUrl, isEmbed) {
-  root.innerHTML = null;
-  root.classList.add("hs-result");
-  render(loaderTemplate(), root);
-  try {
-    const chartData = await fetchDataSet(detailUrl);
-    console.log(chartData, { isEmbed });
-    if (isEmbed) toolbar.classList.remove("toolbar--visible");
-    else toolbar.classList.add("toolbar--visible");
-    render(resultTemplate(templateData(detailUrl, chartData), isEmbed), root);
-  } catch (ex) {
-    console.error(ex);
-    render(notFoundTemplate(), root);
-  }
-}
-
 function invalidURLTemplate() {
   return html`
     <div id="hlidac-shopu-modal__not-found" class="layout-wrapper">
@@ -217,112 +176,73 @@ function invalidURLTemplate() {
   `;
 }
 
-function resultTemplate(
-  {
-    detailUrl,
-    imageUrl,
-    name,
-    shop,
-    lastDeclaredPrice,
-    actualPrice,
-    claimedDiscount,
-    discount,
-    discountType,
-    date,
-    data,
-    ...prices
-  },
-  isEmbed
-) {
-  const crawlDate = x =>
-    x
-      ? html`
-          <time
-            id="latest-date"
-            datetime="${x.toISOString()}"
-            title="Datum posledního čtení cen"
-            >${formatDate(x)}
-          </time>
-        `
-      : null;
-  const shopLogo = x => x && logoTemplate(x);
-  return html`
-    <div id="hlidac-shopu-modal__found" class="layout-wrapper">
-      <h2>
-        ${shopLogo(shops.get(shop))}
-        <a
-          href="${detailUrl}"
-          id="product-name"
-          class="product-name"
-          target="_blank"
-          rel="noopener noreferrer"
-          >${name || "Vámi vybraný produkt"}</a
-        >
-        ${imageUrl ? html`<img alt="${name}" src="${imageUrl}" />` : null}
-      </h2>
-      <div class="box box--purple">
-        ${crawlDate(date)}
-        ${discount !== 0
-          ? originalPriceTemplate({ type: discountType, ...prices })
-          : null}
-        <div class="hs-actual-price">
-          Prodejní cena
-          <span id="current-price">${formatMoney(actualPrice)}</span>
-        </div>
-        ${discountTemplate(
-          {
-            realDiscount: discount,
-            type: discountType,
-            claimedDiscount
-          },
-          true
-        )}
-      </div>
-      <div class="hs-chart">
-        <hs-chart .data="${data}"></hs-chart>
-      </div>
-      ${isEmbed || !navigator.share
-        ? html`
-            <div class="hs-share-buttons">
-              <a
-                class="tw-button"
-                href="${`https://twitter.com/intent/tweet?url=${encodeURIComponent(
-                  location.href
-                )}&hashtags=hlidacshopu`}"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <i></i>
-                <span>Tweet</span>
-              </a>
-              <a
-                class="fb-button"
-                href="${`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
-                  location.href
-                )}`}"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img
-                  class="img"
-                  style="vertical-align:middle"
-                  src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAQAAADZc7J/AAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QA/4ePzL8AAAAHdElNRQfjBBYGJDeRLlUUAAABp0lEQVRIx6WVu04bQRSG/x1jbZCR00RacAJYooksd/RI7qBJHiAlD5F2lQYhkSLFNrwFFVIaUsRRJEspSYnExcRFCguDE1F8FPaavczYXvinmZk955vLmT3HQxaV1dJ7NVRTTVJXXZ3qSCe6t9iSbQERfWzqExFk7dNDn5AB03RDiO8CBLSZR+3kPh7dm5zP5Q5wTjMLCAq4jxBBEuDzw2F4zyfe4lPmJWtspg7iPwJCh3uPrcytJxXGgMB58zu5IKcjEowAkcP9BE0FQIRE2fFs4MNMQJ+yUUtV2fV70tvVdRy1lKpquQ8Alcm6XadNZNSQS7eT3rLTpmFU02x5zi81jxstzXTDCRgYC90bt/xMPU8w6s1xhFhruZnecwF/jDrPAvwy+loAsJqbOfaoqKeK1Xx2FO70yuhWhwX2kNahhkK85r/1mbp+oVhDVpCRdKW9J61/oOu4Lizws/AOvrOQTKp1LgoBuqxm0/oGl3MDztiwFZZ6Jje7AB3euEpbiZB/UwFDPlJy10YhVvg8ztJZwF/2WZ9eXOO2yDZfEuNv7PGOFzbbB6Kd0fTJtKEiAAAAJXRFWHRkYXRlOmNyZWF0ZQAyMDE5LTA0LTIyVDEzOjM2OjU1LTA3OjAwi4nE0AAAACV0RVh0ZGF0ZTptb2RpZnkAMjAxOS0wNC0yMlQxMzozNjo1NS0wNzowMPrUfGwAAAAASUVORK5CYII="
-                  alt=""
-                  width="16"
-                  height="16"
-                />
-                <span>Sdílet</span>
-              </a>
-            </div>
-          `
-        : null}
-    </div>
-  `;
-}
-
 function registerStylesheet(href) {
   const styles = document.createElement("link");
   styles.rel = "stylesheet";
   styles.href = href;
   document.head.insertAdjacentElement("beforeend", styles);
+}
+
+function changeView(root) {
+  root.parentElement.classList.remove("home-screen");
+  root.parentElement.classList.add("result-screen");
+  root.innerHTML = null;
+  root.classList.add("hs-result");
+  render(loaderTemplate(), root);
+}
+
+function renderResults(root, { targetURL, view }) {
+  changeView(root);
+  return Promise.all([fetchDataSet(targetURL), import("./results.js")]).then(
+    ([chartData, { renderResultsModal }]) =>
+      renderResultsModal(targetURL, chartData, {
+        isEmbed: view === "embed",
+        root,
+        toolbar
+      })
+  );
+}
+
+function showInstallBanner() {
+  installBanner.classList.add("hs-install-banner--visible");
+}
+
+function hideInstallBanner() {
+  installBanner.classList.remove("hs-install-banner--visible");
+}
+
+function showHelp() {
+  help.classList.add("help--visible");
+}
+
+function hideShareButton() {
+  shareButton.style.display = "none";
+}
+
+function showOfflineBanner() {
+  progressBar.classList.remove("hs-progress-bar--online");
+  progressBar.classList.add("mdc-top-app-bar--fixed-adjust");
+}
+
+function hideOfflineBanner() {
+  progressBar.classList.add("hs-progress-bar--online");
+  progressBar.classList.remove("mdc-top-app-bar--fixed-adjust");
+}
+
+function showUpdateSnackbar() {
+  snackbarElement.style.display = "block";
+  snackbar.actionButtonText = "Aktualizovat";
+  snackbar.labelText = "K dispozici je nová verze.";
+  snackbar.timeoutMs = -1;
+  snackbar.open();
+}
+
+function disableForm() {
+  const form = document.querySelector(".hs-form");
+  if (form) form.firstElementChild.setAttribute("disabled", "disabled");
+}
+
+function enableForm() {
+  const form = document.querySelector(".hs-form");
+  if (form) form.firstElementChild.removeAttribute("disabled");
 }
---
+++ www.hlidacshopu.cz/src/javascripts/results.js
@@ -0,0 +1,130 @@
+import { html, render } from "lit-html/lit-html.js";
+import { formatDate, formatMoney } from "@hlidac-shopu/lib/format.mjs";
+import {
+  discountTemplate,
+  logoTemplate,
+  notFoundTemplate,
+  originalPriceTemplate
+} from "@hlidac-shopu/lib/templates.mjs";
+import { shops } from "@hlidac-shopu/lib/shops.mjs";
+import { templateData } from "@hlidac-shopu/lib/remoting.mjs";
+import "@hlidac-shopu/lib/web-components/chart.mjs";
+
+export async function renderResultsModal(
+  detailUrl,
+  chartData,
+  { isEmbed, root, toolbar }
+) {
+  try {
+    console.log(chartData, { isEmbed });
+    if (isEmbed) toolbar.classList.remove("toolbar--visible");
+    else toolbar.classList.add("toolbar--visible");
+    render(resultTemplate(templateData(detailUrl, chartData), isEmbed), root);
+  } catch (ex) {
+    console.error(ex);
+    render(notFoundTemplate(), root);
+  }
+}
+
+function resultTemplate(
+  {
+    detailUrl,
+    imageUrl,
+    name,
+    shop,
+    lastDeclaredPrice,
+    actualPrice,
+    claimedDiscount,
+    discount,
+    discountType,
+    date,
+    data,
+    ...prices
+  },
+  isEmbed
+) {
+  const crawlDate = x =>
+    x
+      ? html`
+          <time
+            id="latest-date"
+            datetime="${x.toISOString()}"
+            title="Datum posledního čtení cen"
+            >${formatDate(x)}
+          </time>
+        `
+      : null;
+  const shopLogo = x => x && logoTemplate(x);
+  return html`
+    <div id="hlidac-shopu-modal__found" class="layout-wrapper">
+      <h2>
+        ${shopLogo(shops.get(shop))}
+        <a
+          href="${detailUrl}"
+          id="product-name"
+          class="product-name"
+          target="_blank"
+          rel="noopener noreferrer"
+          >${name || "Vámi vybraný produkt"}</a
+        >
+        ${imageUrl ? html`<img alt="${name}" src="${imageUrl}" />` : null}
+      </h2>
+      <div class="box box--purple">
+        ${crawlDate(date)}
+        ${discount !== 0
+          ? originalPriceTemplate({ type: discountType, ...prices })
+          : null}
+        <div class="hs-actual-price">
+          Prodejní cena
+          <span id="current-price">${formatMoney(actualPrice)}</span>
+        </div>
+        ${discountTemplate(
+          {
+            realDiscount: discount,
+            type: discountType,
+            claimedDiscount
+          },
+          true
+        )}
+      </div>
+      <div class="hs-chart">
+        <hs-chart .data="${data}"></hs-chart>
+      </div>
+      ${isEmbed || !navigator.share
+        ? html`
+            <div class="hs-share-buttons">
+              <a
+                class="tw-button"
+                href="${`https://twitter.com/intent/tweet?url=${encodeURIComponent(
+                  location.href
+                )}&hashtags=hlidacshopu`}"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i></i>
+                <span>Tweet</span>
+              </a>
+              <a
+                class="fb-button"
+                href="${`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
+                  location.href
+                )}`}"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  class="img"
+                  style="vertical-align:middle"
+                  src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAQAAADZc7J/AAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QA/4ePzL8AAAAHdElNRQfjBBYGJDeRLlUUAAABp0lEQVRIx6WVu04bQRSG/x1jbZCR00RacAJYooksd/RI7qBJHiAlD5F2lQYhkSLFNrwFFVIaUsRRJEspSYnExcRFCguDE1F8FPaavczYXvinmZk955vLmT3HQxaV1dJ7NVRTTVJXXZ3qSCe6t9iSbQERfWzqExFk7dNDn5AB03RDiO8CBLSZR+3kPh7dm5zP5Q5wTjMLCAq4jxBBEuDzw2F4zyfe4lPmJWtspg7iPwJCh3uPrcytJxXGgMB58zu5IKcjEowAkcP9BE0FQIRE2fFs4MNMQJ+yUUtV2fV70tvVdRy1lKpquQ8Alcm6XadNZNSQS7eT3rLTpmFU02x5zi81jxstzXTDCRgYC90bt/xMPU8w6s1xhFhruZnecwF/jDrPAvwy+loAsJqbOfaoqKeK1Xx2FO70yuhWhwX2kNahhkK85r/1mbp+oVhDVpCRdKW9J61/oOu4Lizws/AOvrOQTKp1LgoBuqxm0/oGl3MDztiwFZZ6Jje7AB3euEpbiZB/UwFDPlJy10YhVvg8ztJZwF/2WZ9eXOO2yDZfEuNv7PGOFzbbB6Kd0fTJtKEiAAAAJXRFWHRkYXRlOmNyZWF0ZQAyMDE5LTA0LTIyVDEzOjM2OjU1LTA3OjAwi4nE0AAAACV0RVh0ZGF0ZTptb2RpZnkAMjAxOS0wNC0yMlQxMzozNjo1NS0wNzowMPrUfGwAAAAASUVORK5CYII="
+                  alt=""
+                  width="16"
+                  height="16"
+                />
+                <span>Sdílet</span>
+              </a>
+            </div>
+          `
+        : null}
+    </div>
+  `;
+}
