d16752472bc0e669277c92878d6096b4035aea76
Aleš Roubíček <ales@roubicek.name>
2021-01-18 10:00:02 +0100

    Print OS name in update version script

--- scripts/set-apple-version.js
+++ scripts/set-apple-version.js
@@ -3,16 +3,18 @@ const path = require("path");
 const { execSync } = require("child_process");
 
 const extensionPath = path.resolve(__dirname, "../extension");
-const osxPath = path.resolve(__dirname, "../apple/osx/hlidac shopu");
-const iosPath = path.resolve(__dirname, "../apple/ios");
+const paths = new Map([
+  ["macOS", path.resolve(__dirname, "../apple/osx/hlidac shopu")],
+  ["iOS", path.resolve(__dirname, "../apple/ios")]
+]);
 
-for (let cwd of [osxPath, iosPath]) {
+for (let [os, cwd] of paths.entries()) {
   try {
     const { version } = require(`${extensionPath}/manifest.json`);
     execSync(`xcrun agvtool new-marketing-version ${version}`, { cwd });
     execSync(`xcrun agvtool next-version -all`, { cwd }); // this just increments build num
 
-    console.log(`app version is now ${version}`);
+    console.log(`${os} app version is now ${version}`);
   } catch (error) {
     console.error("Failed to set app version", error.stdout.toString("utf8"));
   }
