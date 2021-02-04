88ac6355eb3cec683eb84581dd1e03accd151a9a
Dan Hromada <hromada.dan@gmail.com>
2021-02-04 11:36:08 +0100

    Update Mall scrape selectors aswell

--- extension/shops/mall.mjs
+++ extension/shops/mall.mjs
@@ -13,17 +13,23 @@ export class Mall extends Shop {
   }
 
   async scrape() {
-    const elem = document.querySelector(".price-wrapper, .prices-wrapper");
+    const elem = document.querySelector(
+      ".price-wrapper, .prices-wrapper, .price__wrap"
+    );
     if (!elem) return;
 
     const itemId = document
-      .querySelector('span[data-sel="catalog-number"]')
+      .querySelector(
+        'span[data-sel="catalog-number"], .additional-info__catalog-number span'
+      )
       .innerText.trim()
       .replace("a", "");
     const title = document.querySelector("h1[itemprop=name]").innerText.trim();
-    const currentPrice = cleanPrice("[itemprop=price]");
+    const currentPrice = cleanPrice(
+      "[itemprop=price], .price__wrap__box__final"
+    );
     const originalPrice = cleanPrice(
-      ".old-new-price .rrp-price, .old-price > del:nth-child(1)"
+      ".old-new-price .rrp-price, .old-price > del:nth-child(1), .price__wrap__box__old"
     );
     const imageUrl = document.querySelector(".gallery-magnifier__normal").src;
 
