606f1ab5fe599753c99cec756e3db8a05fc63768
Aleš Roubíček <ales@roubicek.name>
2021-03-02 15:46:21 +0100

    Set year 2020 explicitly on Dashboard

--- www.hlidacshopu.cz/src/javascripts/dashboard.js
+++ www.hlidacshopu.cz/src/javascripts/dashboard.js
@@ -47,7 +47,7 @@ function addExtraData(year) {
 
 addEventListener("DOMContentLoaded", async e => {
   try {
-    const data = await fetchDashboardData();
+    const data = await fetchDashboardData(2020);
     tableRoot.innerHTML = null;
     render(tableTemplate(data), tableRoot);
   } catch (ex) {
