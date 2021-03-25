e0203baae8c047342a423a2b0ea36dc784d065ce
Aleš Roubíček <ales@roubicek.name>
2021-03-25 09:43:29 +0100

    Update Workbox-sw CDN

--- www.hlidacshopu.cz/src/javascripts/sw.js
+++ www.hlidacshopu.cz/src/javascripts/sw.js
@@ -1,5 +1,5 @@
 importScripts(
-  "https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js"
+  "https://storage.googleapis.com/workbox-cdn/releases/6.1.2/workbox-sw.js"
 );
 
 addEventListener("message", event => {
