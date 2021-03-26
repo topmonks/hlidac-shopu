6aa9c2943bdb154366ee7efd4d94242a2f86aeef
Aleš Roubíček <ales@roubicek.name>
2021-03-26 10:06:36 +0100

    [hs-reviews] Refactoring and type annotations

--- actors/hs-reviews/main.js
+++ actors/hs-reviews/main.js
@@ -1,15 +1,19 @@
 const Apify = require("apify");
 const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 
+/** @typedef { import("apify").RequestQueue } RequestQueue */
+/** @typedef { import("apify").RequestList } RequestList */
+/** @typedef { import("apify").CheerioHandlePage } CheerioHandlePage */
+/** @typedef { import("schema-dts").UserReview} UserReview */
+/** @typedef { import("schema-dts").InteractionCounter} InteractionCounter */
+
 const { log } = Apify.utils;
 
-const APPLE = "Apple";
-const APPLE_REVIEWS = "Apple Reviews";
-const FIREFOX = "Firefox";
-const FIREFOX_REVIEWS = "Firefox Reviews";
-const GOOGLE = "Google";
-const GOOGLE_REVIEWS = "Google Reviews";
-
+/**
+ * @param {Request} request
+ * @param {cheerio.Root} $
+ * @returns {InteractionCounter[]}
+ */
 function appleStats(request, $) {
   const reviews = parseInt(
     $(".we-customer-ratings__count").text().match(/\d+/g).pop()
@@ -34,8 +38,14 @@ function appleStats(request, $) {
   ];
 }
 
+/**
+ * @param {cheerio.Root} $
+ */
 function appleReview($) {
-  return function () {
+  return factory;
+
+  /** @returns {UserReview} */
+  function factory() {
     const $review = $(this);
     return {
       "@context": "https://schema.org",
@@ -62,14 +72,24 @@ function appleReview($) {
         "url": "https://www.hlidacshopu.cz/"
       }
     };
-  };
+  }
 }
 
+/**
+ * @param {Request} request
+ * @param {cheerio.Root} $
+ * @returns {UserReview[]}
+ */
 function appleReviews(request, $) {
   const $reviews = $(".we-customer-review");
   return $reviews.map(appleReview($)).toArray();
 }
 
+/**
+ * @param {Request} request
+ * @param {cheerio.Root} $
+ * @returns {InteractionCounter[]}
+ */
 function googleStats(request, $) {
   const downloads = parseInt(
     $(".left-panel > div:nth-child(2) > div:nth-child(2) > .value")
@@ -118,12 +138,18 @@ function googleStats(request, $) {
   ];
 }
 
+/**
+ * @param {cheerio.Root} $
+ */
 function googleReview($) {
-  return function () {
+  return factory;
+
+  /** @returns {UserReview} */
+  function factory() {
     const $review = $(this);
     return {
       "@context": "https://schema.org",
-      "@type": "Review",
+      "@type": "UserReview",
       "author": {
         "@type": "Person",
         "name": $review.find("[itemprop=author] [itemprop=name]").text(),
@@ -144,14 +170,24 @@ function googleReview($) {
         "url": "https://www.hlidacshopu.cz/"
       }
     };
-  };
+  }
 }
 
+/**
+ * @param {Request} request
+ * @param {cheerio.Root} $
+ * @returns {UserReview[]}
+ */
 function googleReviews(request, $) {
   const $reviews = $("table.table tbody tr");
   return $reviews.map(googleReview($)).toArray();
 }
 
+/**
+ * @param {Request} request
+ * @param {cheerio.Root} $
+ * @returns {InteractionCounter[]}
+ */
 function firefoxStats(request, $) {
   const meta = $(".MetadataCard > dl");
   if (!meta.length) return [];
@@ -199,10 +235,11 @@ function firefoxStats(request, $) {
   ];
 }
 
+/** @returns {UserReview} */
 function firefoxReview(result) {
   return {
     "@context": "https://schema.org",
-    "@type": "Review",
+    "@type": "UserReview",
     "@id": result.id,
     "author": {
       "@type": "Person",
@@ -227,6 +264,12 @@ function firefoxReview(result) {
   };
 }
 
+/**
+ * @param {Request} request
+ * @param {*} json
+ * @param {RequestQueue} requestQueue
+ * @returns {UserReview[]}
+ */
 async function firefoxReviews(request, json, requestQueue) {
   if (json.next) {
     await requestQueue.addRequest({
@@ -237,6 +280,12 @@ async function firefoxReviews(request, json, requestQueue) {
   return json.results.map(firefoxReview);
 }
 
+/**
+ * @param {S3Client} s3
+ * @param {string} fileName
+ * @param {string} ext
+ * @param {*} data
+ */
 async function uploadToS3(s3, fileName, ext, data) {
   await s3.send(
     new PutObjectCommand({
@@ -248,99 +297,7 @@ async function uploadToS3(s3, fileName, ext, data) {
   );
 }
 
-Apify.main(async () => {
-  const input = await Apify.getInput();
-  const { awsCredentials } = input ?? {};
-
-  const requestList = await Apify.openRequestList("urls", [
-    {
-      url:
-        "https://apps.apple.com/cz/app/hl%C3%ADda%C4%8D-shop%C5%AF/id1488295734?l=cs",
-      userData: { label: APPLE }
-    },
-    {
-      url:
-        "https://apps.apple.com/cz/app/hl%C3%ADda%C4%8D-shop%C5%AF/id1488295734?l=cs",
-      userData: { label: APPLE_REVIEWS }
-    },
-    {
-      url: "https://chrome-stats.com/d/plmlonggbfebcjelncogcnclagkmkikk",
-      userData: { label: GOOGLE }
-    },
-    {
-      url:
-        "https://chrome-stats.com/d/plmlonggbfebcjelncogcnclagkmkikk/reviews",
-      userData: { label: GOOGLE_REVIEWS }
-    },
-    {
-      url:
-        "https://addons.mozilla.org/en-US/firefox/addon/hl%C3%ADda%C4%8D-shop%C5%AF/",
-      userData: { label: FIREFOX }
-    },
-    {
-      url: "https://addons.mozilla.org/api/v4/ratings/rating/?addon=1013407",
-      userData: { label: FIREFOX_REVIEWS }
-    }
-  ]);
-  const requestQueue = await Apify.openRequestQueue();
-
-  const reviews = [];
-  const stats = [];
-
-  const crawler = new Apify.CheerioCrawler({
-    requestList,
-    requestQueue,
-    maxRequestRetries: 10,
-    additionalMimeTypes: ["application/json", "text/plain"],
-    handleFailedRequestFunction: async ({ request }) => {
-      log.error(`Request ${request.url} failed multiple times`, request);
-    },
-    handlePageFunction: async ({ request, $, json }) => {
-      switch (request.userData.label) {
-        case APPLE: {
-          const result = appleStats(request, $);
-          await Apify.pushData(result);
-          stats.push(...result);
-          break;
-        }
-        case APPLE_REVIEWS: {
-          const result = appleReviews(request, $);
-          await Apify.pushData(result);
-          reviews.push(...result);
-          break;
-        }
-        case GOOGLE: {
-          const result = googleStats(request, $);
-          await Apify.pushData(result);
-          stats.push(...result);
-          break;
-        }
-        case GOOGLE_REVIEWS: {
-          const result = googleReviews(request, $);
-          await Apify.pushData(result);
-          reviews.push(...result);
-          break;
-        }
-        case FIREFOX: {
-          const result = firefoxStats(request, $);
-          await Apify.pushData(result);
-          stats.push(...result);
-          break;
-        }
-        case FIREFOX_REVIEWS: {
-          const result = await firefoxReviews(request, json, requestQueue);
-          await Apify.pushData(result);
-          reviews.push(...result);
-          break;
-        }
-      }
-    }
-  });
-
-  log.info("CRAWLER: scraping reviews");
-  await crawler.run();
-  log.info("CRAWLER: done");
-
+async function saveData({ awsCredentials, reviews, stats }) {
   log.info("S3: starting upload");
   const s3 = new S3Client({
     region: "eu-central-1",
@@ -349,4 +306,129 @@ Apify.main(async () => {
   await uploadToS3(s3, "reviews", "jsonld", reviews);
   await uploadToS3(s3, "stats", "jsonld", stats);
   log.info("S3: done");
+}
+
+const APPLE = "Apple";
+const APPLE_REVIEWS = "Apple Reviews";
+const FIREFOX = "Firefox";
+const FIREFOX_REVIEWS = "Firefox Reviews";
+const GOOGLE = "Google";
+const GOOGLE_REVIEWS = "Google Reviews";
+
+const requests = new Map([
+  [
+    APPLE,
+    "https://apps.apple.com/cz/app/hl%C3%ADda%C4%8D-shop%C5%AF/id1488295734?l=cs"
+  ],
+  [
+    APPLE_REVIEWS,
+    "https://apps.apple.com/cz/app/hl%C3%ADda%C4%8D-shop%C5%AF/id1488295734?l=cs"
+  ],
+  [GOOGLE, "https://chrome-stats.com/d/plmlonggbfebcjelncogcnclagkmkikk"],
+  [
+    GOOGLE_REVIEWS,
+    "https://chrome-stats.com/d/plmlonggbfebcjelncogcnclagkmkikk/reviews"
+  ],
+  [
+    FIREFOX,
+    "https://addons.mozilla.org/en-US/firefox/addon/hl%C3%ADda%C4%8D-shop%C5%AF/"
+  ],
+  [
+    FIREFOX_REVIEWS,
+    "https://addons.mozilla.org/api/v4/ratings/rating/?addon=1013407"
+  ]
+]);
+
+/**
+ * @param {RequestQueue} requestQueue
+ */
+function createHandlePageFunction(requestQueue) {
+  /** @type {UserReview[]} */
+  const reviews = [];
+  /** @type {InteractionCounter[]} */
+  const stats = [];
+  /** @type {CheerioHandlePage} */
+  const handlePageFunction = async ({ request, $, json }) => {
+    log.info(`Handling page ${request.url}`);
+    switch (request.userData.label) {
+      case APPLE: {
+        const result = appleStats(request, $);
+        await Apify.pushData(result);
+        stats.push(...result);
+        break;
+      }
+      case APPLE_REVIEWS: {
+        const result = appleReviews(request, $);
+        await Apify.pushData(result);
+        reviews.push(...result);
+        break;
+      }
+      case GOOGLE: {
+        const result = googleStats(request, $);
+        await Apify.pushData(result);
+        stats.push(...result);
+        break;
+      }
+      case GOOGLE_REVIEWS: {
+        const result = googleReviews(request, $);
+        await Apify.pushData(result);
+        reviews.push(...result);
+        break;
+      }
+      case FIREFOX: {
+        const result = firefoxStats(request, $);
+        await Apify.pushData(result);
+        stats.push(...result);
+        break;
+      }
+      case FIREFOX_REVIEWS: {
+        const result = await firefoxReviews(request, json, requestQueue);
+        await Apify.pushData(result);
+        reviews.push(...result);
+        break;
+      }
+    }
+  };
+
+  return { reviews, stats, handlePageFunction };
+}
+
+Apify.main(async () => {
+  const input = await Apify.getInput();
+  const { awsCredentials } = input ?? {};
+
+  /** @type {RequestList} */
+  const requestList = await Apify.openRequestList(
+    "urls",
+    Array.from(requests.entries()).map(([label, url]) => ({
+      url,
+      userData: { label }
+    }))
+  );
+  /** @type {RequestQueue} */
+  const requestQueue = await Apify.openRequestQueue();
+  const { reviews, stats, handlePageFunction } = createHandlePageFunction(
+    requestQueue
+  );
+
+  const crawler = new Apify.CheerioCrawler({
+    requestList,
+    requestQueue,
+    handlePageFunction,
+    maxRequestRetries: 10,
+    additionalMimeTypes: ["application/json", "text/plain"],
+    async handleFailedRequestFunction({ request }) {
+      log.error(`Request ${request.url} failed multiple times`, request);
+    }
+  });
+
+  log.info("CRAWLER: scraping reviews");
+  await crawler.run();
+  log.info("CRAWLER: done");
+
+  await saveData({
+    awsCredentials,
+    reviews,
+    stats
+  });
 });
