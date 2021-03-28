2d767d4f8b656ae7953478066743906b7f96a1da
Aleš Roubíček <ales@roubicek.name>
2021-03-28 16:11:15 +0200

    [hs-reviews] Added Apple Downloads stats

--- actors/hs-reviews/main.js
+++ actors/hs-reviews/main.js
@@ -1,13 +1,21 @@
-const Apify = require("apify");
 const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const Apify = require("apify");
+const cheerio = require("cheerio");
+const csv = require("csv-parse/lib/sync");
+const jwt = require("jsonwebtoken");
+const fetch = require("node-fetch");
+const { URLSearchParams } = require("url");
+const { promisify } = require("util");
+const zlib = require("zlib");
 
 /** @typedef { import("apify").RequestQueue } RequestQueue */
 /** @typedef { import("apify").RequestList } RequestList */
-/** @typedef { import("apify").CheerioHandlePage } CheerioHandlePage */
+/** @typedef { import("apify").HandleRequest } HandleRequest */
 /** @typedef { import("schema-dts").UserReview} UserReview */
 /** @typedef { import("schema-dts").InteractionCounter} InteractionCounter */
 
-const { log } = Apify.utils;
+const gunzip = promisify(zlib.gunzip);
+const { log, requestAsBrowser } = Apify.utils;
 
 /**
  * @param {Request} request
@@ -30,6 +38,49 @@ function appleStats(request, $) {
           "https://apps.apple.com/cz/app/hl%C3%ADda%C4%8D-shop%C5%AF/id1488295734?l=cs#?platform=mac"
       },
       "userInteractionCount": reviews,
+      "subjectOf": {
+        "@type": "WebApplication",
+        "url": "https://www.hlidacshopu.cz/"
+      }
+    }
+  ];
+}
+
+/**
+ * @param {Request} request
+ * @param {Response} resp
+ * @param {RequestQueue} requestQueue
+ * @returns {InteractionCounter[]}
+ */
+async function appleDownloads(request, resp, requestQueue) {
+  if (request.userData.year < new Date().getFullYear()) {
+    const year = request.userData.year + 1;
+    await requestQueue.addRequest({
+      url: appleDownloadsUrl(year),
+      userData: { ...request.userData, year }
+    });
+  }
+  const data = await resp
+    .buffer()
+    .then(gunzip)
+    .then(x => csv(x.toString(), { delimiter: "\t", columns: true }));
+  const downloads = data
+    .filter(x => x["Apple Identifier"] === "1488295734")
+    .map(x => parseInt(x["Units"]))
+    .reduce((acc, x) => acc + x, 0);
+  return [
+    {
+      "@context": "https://schema.org",
+      "@type": "InteractionCounter",
+      "interactionType": "https:/schema.org/InstallAction",
+      "interactionService": {
+        "@type": "WebSite",
+        "name": "Apple App Store",
+        "url":
+          "https://apps.apple.com/cz/app/hl%C3%ADda%C4%8D-shop%C5%AF/id1488295734?l=cs#?platform=mac"
+      },
+      "disambiguatingDescription": request.userData.year.toString(),
+      "userInteractionCount": downloads,
       "subjectOf": {
         "@type": "WebApplication",
         "url": "https://www.hlidacshopu.cz/"
@@ -310,10 +361,23 @@ async function saveData({ awsCredentials, reviews, stats }) {
 
 const APPLE = "Apple";
 const APPLE_REVIEWS = "Apple Reviews";
+const APPLE_DOWNLOADS = "Apple Downloads";
 const FIREFOX = "Firefox";
 const FIREFOX_REVIEWS = "Firefox Reviews";
 const GOOGLE = "Google";
 const GOOGLE_REVIEWS = "Google Reviews";
+
+function appleDownloadsUrl(year) {
+  return `https://api.appstoreconnect.apple.com/v1/salesReports?${new URLSearchParams(
+    {
+      "filter[frequency]": "YEARLY",
+      "filter[reportDate]": year.toString(),
+      "filter[reportSubType]": "SUMMARY",
+      "filter[reportType]": "SALES",
+      "filter[vendorNumber]": "85389739"
+    }
+  )}`;
+}
 
 const requests = new Map([
   [
@@ -324,6 +388,7 @@ const requests = new Map([
     APPLE_REVIEWS,
     "https://apps.apple.com/cz/app/hl%C3%ADda%C4%8D-shop%C5%AF/id1488295734?l=cs"
   ],
+  [APPLE_DOWNLOADS, appleDownloadsUrl(2019)],
   [GOOGLE, "https://chrome-stats.com/d/plmlonggbfebcjelncogcnclagkmkikk"],
   [
     GOOGLE_REVIEWS,
@@ -341,48 +406,76 @@ const requests = new Map([
 
 /**
  * @param {RequestQueue} requestQueue
+ * @param {String} token
  */
-function createHandlePageFunction(requestQueue) {
+function createHandlePageFunction(requestQueue, token) {
   /** @type {UserReview[]} */
   const reviews = [];
   /** @type {InteractionCounter[]} */
   const stats = [];
-  /** @type {CheerioHandlePage} */
-  const handlePageFunction = async ({ request, $, json }) => {
+  /** @type {HandleRequest} */
+  const handlePageFunction = async ({ request }) => {
     log.info(`Handling page ${request.url}`);
     switch (request.userData.label) {
       case APPLE: {
+        const response = await requestAsBrowser(request);
+        const $ = cheerio.load(response.body);
         const result = appleStats(request, $);
         await Apify.pushData(result);
         stats.push(...result);
         break;
       }
+      case APPLE_DOWNLOADS: {
+        const resp = await fetch(request.url, {
+          headers: {
+            "Authorization": `Bearer ${token}`,
+            "Accept": "application/a-gzip"
+          }
+        });
+        const result = await appleDownloads(request, resp, requestQueue);
+        await Apify.pushData(result);
+        stats.push(...result);
+        break;
+      }
       case APPLE_REVIEWS: {
+        const response = await requestAsBrowser(request);
+        const $ = cheerio.load(response.body);
         const result = appleReviews(request, $);
         await Apify.pushData(result);
         reviews.push(...result);
         break;
       }
       case GOOGLE: {
+        const response = await requestAsBrowser(request);
+        const $ = cheerio.load(response.body);
         const result = googleStats(request, $);
         await Apify.pushData(result);
         stats.push(...result);
         break;
       }
       case GOOGLE_REVIEWS: {
+        const response = await requestAsBrowser(request);
+        const $ = cheerio.load(response.body);
         const result = googleReviews(request, $);
         await Apify.pushData(result);
         reviews.push(...result);
         break;
       }
       case FIREFOX: {
+        const response = await requestAsBrowser(request);
+        const $ = cheerio.load(response.body);
         const result = firefoxStats(request, $);
         await Apify.pushData(result);
         stats.push(...result);
         break;
       }
       case FIREFOX_REVIEWS: {
-        const result = await firefoxReviews(request, json, requestQueue);
+        const response = await requestAsBrowser(request);
+        const result = await firefoxReviews(
+          request,
+          JSON.parse(response.body),
+          requestQueue
+        );
         await Apify.pushData(result);
         reviews.push(...result);
         break;
@@ -393,30 +486,50 @@ function createHandlePageFunction(requestQueue) {
   return { reviews, stats, handlePageFunction };
 }
 
+/**
+ * Creates JWT for Apple API request authorization
+ * @param {String} applePK
+ * @returns {String}
+ */
+function createAppleJWT(applePK) {
+  return jwt.sign({}, applePK, {
+    "algorithm": "ES256",
+    "issuer": "69a6de74-fd57-47e3-e053-5b8c7c11a4d1",
+    "expiresIn": 1000,
+    "audience": "appstoreconnect-v1",
+    header: {
+      "alg": "ES256",
+      "kid": "JHF22PT4Z8",
+      "typ": "JWT"
+    }
+  });
+}
+
 Apify.main(async () => {
   const input = await Apify.getInput();
-  const { awsCredentials } = input ?? {};
+  const { awsCredentials, applePK } = input ?? {};
+  const token = createAppleJWT(applePK);
 
   /** @type {RequestList} */
   const requestList = await Apify.openRequestList(
     "urls",
     Array.from(requests.entries()).map(([label, url]) => ({
       url,
-      userData: { label }
+      userData: { label, year: 2019 }
     }))
   );
   /** @type {RequestQueue} */
   const requestQueue = await Apify.openRequestQueue();
   const { reviews, stats, handlePageFunction } = createHandlePageFunction(
-    requestQueue
+    requestQueue,
+    token
   );
 
-  const crawler = new Apify.CheerioCrawler({
+  const crawler = new Apify.BasicCrawler({
     requestList,
     requestQueue,
-    handlePageFunction,
-    maxRequestRetries: 10,
-    additionalMimeTypes: ["application/json", "text/plain"],
+    handleRequestFunction: handlePageFunction,
+    maxRequestRetries: 1,
     async handleFailedRequestFunction({ request }) {
       log.error(`Request ${request.url} failed multiple times`, request);
     }
