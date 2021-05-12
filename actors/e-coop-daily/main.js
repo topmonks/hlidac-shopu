d76a0b888ec5da3f856312b47048875dfa0ddce3
Jan Fiedler <janfiedler@gmail.com>
2021-05-12 16:54:20 +0200

    [e-coop-daily] - Fixing scrapping from apify sdk 0.22 to 1.2.0 - Apify.CheerioCrawler handlePageFunction code optimalization - Disable PROXY due to (bug?) in APIFY 1.2.0

--- actors/e-coop-daily/main.js
+++ actors/e-coop-daily/main.js
@@ -6,44 +6,156 @@ const { LABELS, MARKETS_URL, COOP_BOX_CATEGORY_POST } = require("./src/const");
 
 const { log, requestAsBrowser } = Apify.utils;
 
+const stats = async () => {
+  return (
+    (await Apify.getValue("STATS")) || {
+      markets: 0,
+      items: 0
+    }
+  );
+};
+const uniqueItemId = new Set();
+
+/**
+ * Creates Page Function for scraping
+ * @param {RequestQueue} requestQueue
+ * @param {S3Client} s3
+ * @returns {CheerioHandlePage}
+ */
+function pageFunction(requestQueue) {
+  const processedIds = new Set();
+
+  /**
+   *  @param {CheerioHandlePageInputs} context
+   *  @returns {Promise<void>}
+   */
+  async function handler(context) {
+    const { $, body, contentType, request, response } = context;
+    const { label, category, currentPage } = request.userData;
+    log.debug(`Start scraping label: [${label}] url: [${request.url}]`);
+    let requests = [];
+    let item = null;
+    let items = [];
+    switch (label) {
+      case LABELS.START:
+        const bodyJson = JSON.parse(body);
+        for (const market of bodyJson) {
+          if (market.website && market.website.includes("http")) {
+            await requestQueue.addRequest({
+              url: market.website.includes("coop-box")
+                ? "https://eshop.coop-box.cz/"
+                : market.website,
+              userData: {
+                label: market.website.includes("coop-box")
+                  ? LABELS.COOP_BOX
+                  : LABELS.MARKET,
+                marketTitle: market.title,
+                marketId: market.id
+              }
+            });
+            //break;
+          }
+        }
+        break;
+      case LABELS.COOP_BOX:
+        log.debug("COOP-BOX market");
+        requests = extractor.extractCoopBoxCategories($, request);
+        break;
+      case LABELS.COOP_BOX_CATEGORY:
+        await requestQueue.addRequest({
+          method: "POST",
+          url: request.url,
+          payload: JSON.stringify(
+            COOP_BOX_CATEGORY_POST(request.userData.sourceId, "PB_MENU")
+          ),
+          userData: {
+            label: LABELS.COOP_BOX_CATEGORY_RESPONSE
+          }
+        });
+        break;
+      case LABELS.COOP_BOX_CATEGORY_RESPONSE:
+        if (response.statusCode === 200) {
+          requests = extractor.extractCoopBoxPages($, request);
+          items = extractor.extractCoopBoxItems($, request);
+        }
+        break;
+      case LABELS.COOP_BOX_NEXT_PAGE:
+        await requestQueue.addRequest({
+          method: "POST",
+          url: request.url,
+          payload: JSON.stringify(
+            COOP_BOX_CATEGORY_POST(request.userData.sourceId)
+          ),
+          userData: {
+            label: LABELS.COOP_BOX_NEXT_PAGE_RESPONSE
+          }
+        });
+        break;
+      case LABELS.COOP_BOX_NEXT_PAGE_RESPONSE:
+        if (response.statusCode === 200) {
+          requests = extractor.extractCoopBoxPages($, request);
+          items = extractor.extractCoopBoxItems($, request);
+        }
+        break;
+      case LABELS.MARKET:
+        requests = extractor.extractMainCategories($, request);
+        stats.markets++;
+        log.debug(`Found ${requests.length} main categories`);
+        break;
+      case LABELS.MAIN_CATEGORY:
+        requests = extractor.extractCategories($, request);
+        log.debug(`Found ${requests.length} categories`);
+        break;
+      case LABELS.CATEGORY:
+        requests = extractor.extractPages($, request);
+        requests = requests.concat(extractor.extractItemDetails($, request));
+        break;
+      case LABELS.DETAIL:
+        item = extractor.extractItem($, request);
+        stats.items++;
+        await Apify.pushData(item);
+        break;
+    }
+    for (const r of requests) {
+      await requestQueue.addRequest(r, { forefront: true });
+    }
+    for (const i of items) {
+      if (!uniqueItemId.has(i.itemId)) {
+        uniqueItemId.add(i.itemId);
+        await Apify.pushData(i);
+      }
+    }
+  }
+  return handler;
+}
+
 Apify.main(async () => {
   log.info("ACTOR - start");
-  const { development = false } = await Apify.getInput();
+  const input = await Apify.getInput();
+  const {
+    development,
+    maxRequestRetries = 1,
+    country = "cz",
+    proxyGroups = ["CZECH_LUMINATI"]
+  } = input ?? {};
   if (development) {
     Apify.utils.log.setLevel(Apify.utils.log.LEVELS.DEBUG);
   }
 
-  const stats = (await Apify.getValue("STATS")) || {
-    markets: 0,
-    items: 0
-  };
   const requestQueue = await Apify.openRequestQueue();
-  const proxyConfiguration = await Apify.createProxyConfiguration();
-
-  const marketsRequest = await requestAsBrowser({
-    url: MARKETS_URL,
-    proxyUrl: proxyConfiguration.newUrl(),
-    json: true
+  /** @type {ProxyConfiguration} */
+  const proxyConfiguration = await Apify.createProxyConfiguration({
+    groups: proxyGroups,
+    useApifyProxy: false
   });
-  if (marketsRequest.statusCode === 200) {
-    const { body } = marketsRequest;
-    for (const market of body) {
-      if (market.website && market.website.includes("http")) {
-        await requestQueue.addRequest({
-          url: market.website.includes("coop-box")
-            ? "https://eshop.coop-box.cz/"
-            : market.website,
-          userData: {
-            label: market.website.includes("coop-box")
-              ? LABELS.COOP_BOX
-              : LABELS.MARKET,
-            marketTitle: market.title,
-            marketId: market.id
-          }
-        });
-      }
+
+  await requestQueue.addRequest({
+    url: MARKETS_URL,
+    userData: {
+      label: "START"
     }
-  }
+  });
+
   log.info("ACTOR - setUp crawler");
   const persistState = async () => {
     await Apify.setValue("STATS", stats).then(() => log.debug("STATS saved!"));
@@ -51,83 +163,13 @@ Apify.main(async () => {
   };
   Apify.events.on("persistState", persistState);
 
-  const uniqueItemId = new Set();
   const crawler = new Apify.CheerioCrawler({
     requestQueue,
-    maxRequestRetries: 10,
     proxyConfiguration,
-    handlePageFunction: async ({ request, $ }) => {
-      const { label } = request.userData;
-      log.debug(`Start scraping label: [${label}] url: [${request.url}]`);
-      let requests = [];
-      let item = null;
-      let items = [];
-      let response = null;
-      switch (label) {
-        case LABELS.COOP_BOX:
-          log.debug("COOP-BOX market");
-          requests = extractor.extractCoopBoxCategories($, request);
-          break;
-        case LABELS.COOP_BOX_CATEGORY:
-          response = await requestAsBrowser({
-            method: "POST",
-            url: request.url,
-            proxyUrl: proxyConfiguration.newUrl(),
-            payload: JSON.stringify(
-              COOP_BOX_CATEGORY_POST(request.userData.sourceId, "PB_MENU")
-            )
-          });
-          if (response.statusCode === 200) {
-            const { body } = response;
-            $ = load(body);
-            requests = extractor.extractCoopBoxPages($, request);
-            items = extractor.extractCoopBoxItems($, request);
-          }
-          break;
-        case LABELS.COOP_BOX_NEXT_PAGE:
-          response = await requestAsBrowser({
-            method: "POST",
-            url: request.url,
-            proxyUrl: proxyConfiguration.newUrl(),
-            payload: JSON.stringify(
-              COOP_BOX_CATEGORY_POST(request.userData.sourceId)
-            )
-          });
-          if (response.statusCode === 200) {
-            const { body } = response;
-            $ = load(body);
-            requests = extractor.extractCoopBoxPages($, request);
-            items = extractor.extractCoopBoxItems($, request);
-          }
-          break;
-        case LABELS.MARKET:
-          requests = extractor.extractMainCategories($, request);
-          stats.markets++;
-          log.debug(`Found ${requests.length} main categories`);
-          break;
-        case LABELS.MAIN_CATEGORY:
-          requests = extractor.extractCategories($, request);
-          log.debug(`Found ${requests.length} categories`);
-          break;
-        case LABELS.CATEGORY:
-          requests = extractor.extractPages($, request);
-          requests = requests.concat(extractor.extractItemDetails($, request));
-          break;
-        case LABELS.DETAIL:
-          item = extractor.extractItem($, request);
-          stats.items++;
-          await Apify.pushData(item);
-          break;
-      }
-      for (const r of requests) {
-        await requestQueue.addRequest(r, { forefront: true });
-      }
-      for (const i of items) {
-        if (!uniqueItemId.has(i.itemId)) {
-          uniqueItemId.add(i.itemId);
-          await Apify.pushData(i);
-        }
-      }
+    maxRequestRetries,
+    handlePageFunction: pageFunction(requestQueue),
+    handleFailedRequestFunction: async ({ request }) => {
+      log.error(`Request ${request.url} failed multiple times`, request);
     }
   });
 
@@ -136,7 +178,6 @@ Apify.main(async () => {
   await crawler.run();
 
   log.info("ACTOR - crawler end");
-
   // stats page
   try {
     const env = await Apify.getEnv();
