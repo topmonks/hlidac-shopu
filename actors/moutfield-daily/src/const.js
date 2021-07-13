29c88c269c7f61cff750191ccb5a863f1a473718
Jan Fiedler <janfiedler@gmail.com>
2021-07-13 10:43:09 +0200

    [mountfield-daily] - Fix code style with prettier

--- actors/moutfield-daily/src/const.js
+++ actors/moutfield-daily/src/const.js
@@ -1,20 +1,20 @@
-const WEB = 'https://www.mountfield.cz';
-const WEB_SK = 'https://www.mountfield.sk';
+const WEB = "https://www.mountfield.cz";
+const WEB_SK = "https://www.mountfield.sk";
 const COUNTRY = {
-  CZ: 'CZ',
-  SK: 'SK',
+  CZ: "CZ",
+  SK: "SK"
 };
 const LABELS = {
-  START: 'START',
-  MAIN_CATEGORY: 'MAIN_CATEGORY',
-  SUB_CATEGORY: 'SUB_CATEGORY',
+  START: "START",
+  MAIN_CATEGORY: "MAIN_CATEGORY",
+  SUB_CATEGORY: "SUB_CATEGORY"
 };
-const BF = 'BF';
+const BF = "BF";
 
 module.exports = {
   WEB,
   WEB_SK,
   COUNTRY,
   LABELS,
-  BF,
-}
+  BF
+};
