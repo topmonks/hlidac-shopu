631148eb96672b5dfc725f9a01d56482e8a6f4e8
Lan Vu <lan.vu@topmonks.com>
2021-07-26 16:54:43 +0200

    [obi-daily] - deploy

--- actors/obi-daily/main.js
+++ actors/obi-daily/main.js
@@ -10,22 +10,21 @@ const rollbar = require("@hlidac-shopu/actors-common/rollbar.js");
 const Apify = require("apify");
 const { log } = Apify.utils;
 
-const COUNTRY = {
-  CZ: "CZ",
-  SK: "SK",
-  PL: "PL",
-  HU: "HU",
-  IT: "IT", // obi-italia.it
-  DE: "DE",
-  AT: "AT",
-  RU: "RU",
-  CH: "CH"
-};
+// const COUNTRY = {
+//   CZ: "CZ",
+//   SK: "SK",
+//   PL: "PL",
+//   HU: "HU",
+//   IT: "IT", // obi-italia.it
+//   DE: "DE",
+//   AT: "AT",
+//   RU: "RU",
+//   CH: "CH"
+// };
 
-// function s3FileName(detail) {
-//   const url = new URL(detail.itemUrl);
-//   return url.pathname.match(/-p(\d+)\.html$/)?.[1];
-// }
+const processedIds = new Set();
+let stats = {};
+let pushList = [];
 
 function getHomePageUrl() {
   return global.homePageUrl;
@@ -42,9 +41,19 @@ const handleStart = async ({ $, requestQueue, request }) => {
       };
     })
     .get();
+  if (!(categoryLinkList.length > 0)) {
+    categoryLinkList = $("ul.first-level > li > a")
+      .map(function () {
+        return {
+          href: $(this).attr("href")
+          // dataWebtrekk: $(this).attr("wt_name")
+        };
+      })
+      .get();
+  }
   log.debug(
-    `[handleStart] label: ${
-      request.userData.label
+    `[handleStart] label: ${request.userData.label}, url: ${
+      request.url
     }, subcategories: ${JSON.stringify(categoryLinkList)}`
   );
   if (global.inputData.development) {
@@ -63,6 +72,7 @@ const handleStart = async ({ $, requestQueue, request }) => {
         url: categoryUrl,
         userData: { label: "SUBCAT" }
       });
+      stats.urls += 1;
     }
   }
 };
@@ -97,6 +107,7 @@ async function handleSubCategory(context) {
         url: subcategoryUrl,
         userData: { label: label + "I" }
       });
+      stats.urls += 1;
     }
   }
 }
@@ -131,6 +142,7 @@ async function handleLastSubCategory(context) {
         });
       });
     await Promise.all(requestList);
+    stats.urls += requestList.length;
   }
   await handleList(context);
 }
@@ -141,8 +153,16 @@ async function handleList({ $, requestQueue, request }) {
       return $(this).attr("href");
     })
     .get();
+  log.debug(
+    `[handleList] label: ${request.userData.label}, url: ${
+      request.url
+    }, productLinkList: ${JSON.stringify(productLinkList)}`
+  );
   if (global.inputData.development) {
     productLinkList = productLinkList.slice(0, 1);
+    log.debug(
+      `development mode, productLinkList: ${JSON.stringify(productLinkList)}`
+    );
   }
   const homePageUrl = getHomePageUrl();
   const requestList = productLinkList.map(url => {
@@ -152,50 +172,111 @@ async function handleList({ $, requestQueue, request }) {
       userData: { label: "DETAIL" }
     });
   });
-  log.debug(
-    `[handleList] ${request.url}, productDetailCount ${requestList.length}`
-  );
   await Promise.all(requestList);
+  stats.urls += requestList.length;
 }
-//
-// function getProductAttribute($){
-//   const scriptTextList = $('script').map(function () {
-//     return $(this).html();
-//   }).get();
-//   const variableDataLayer = "dataLayer =";
-//   const variableWtConfig = "var wtConfig =";
-//   let dataLayer;
-//   let wtConfig;
-//   const productAttribute = {};
-//   for (const scriptText of scriptTextList) {
-//     const foundDataLayer = scriptText.match(variableDataLayer);
-//     if (foundDataLayer) {
-//       const startIndex = scriptText.search(variableDataLayer);
-//       const endIndex = scriptText.search(";");
-//       if (startIndex != -1 && endIndex != -1) {
-//         const value = scriptText.substring(startIndex + variableDataLayer.length, endIndex);
-//         dataLayer = JSON.parse(value);
-//       }
-//     }
-//     const foundWtConfig = scriptText.match(variableWtConfig);
-//     if (foundWtConfig) {
-//       const startIndex1 = scriptText.search(variableWtConfig);
-//       const endIndex1 = scriptText.search("};");
-//       if (startIndex1 != -1 && endIndex1 != -1) {
-//         const value1 = scriptText.substring(startIndex1 + variableWtConfig.length, endIndex1);
-//         wtConfig = JSON.parse(value1);
-//       }
-//     }
-//     if (dataLayer && wtConfig) {
-//       break;
-//     }
-//   }
-//   return {dataLayer, wtConfig};
-// }
 
-// async function handleDetail({ request }) {
-//   log.debug(`handleDetail ${request.url}`);
-// }
+async function handleDetail({ request, $ }, s3, country) {
+  log.debug(
+    `[handleDetail] label: ${request.userData.label}, url: ${request.url}`
+  );
+
+  const itemName = $(".overview__description >.overview__heading")
+    .text()
+    .trim();
+  const itemId = $('input[name="code"]').attr("value").trim();
+  let currency = $('meta[itemprop="priceCurrency"]')
+    .map(function () {
+      return $(this).attr("content");
+    })
+    .get()[0];
+  if (currency === "SKK") {
+    currency = "EUR";
+  }
+  let currentPrice = $('[data-ui-name="ads.price.strong"]').text();
+  currentPrice = parsePrice(currentPrice);
+  let discountedPrice = $(".saving").get(0);
+  let originalPrice = currentPrice;
+  if (discountedPrice) {
+    originalPrice = $(discountedPrice.parent.children)
+      .map(function () {
+        const el = $(this);
+        const tagName = el.get(0).tagName;
+        if (tagName === "del") {
+          const text = el.text();
+          if (text.match(/\d/)) {
+            return text;
+          }
+        }
+      })
+      .get(0);
+    originalPrice = parsePrice(originalPrice);
+    discountedPrice = originalPrice - currentPrice;
+  }
+  const discounted = !!discountedPrice;
+  const inStock = !!$("div.marg_b5").text().match(/(\d+)/);
+  let img = $(".ads-slider__link").attr("href");
+  if (!img) {
+    img = $(".ads-slider__image")
+      .map(function () {
+        return $(this).attr("data-src");
+      })
+      .get(0);
+  }
+  img = `https:${img}`;
+  const category = $("li.breadcrumb__dropdown__wrapper > a")
+    .map(function () {
+      return $(this).text();
+    })
+    .get()
+    .join("/");
+  const result = {
+    itemUrl: request.url,
+    itemName,
+    itemId,
+    currency,
+    currentPrice,
+    discounted,
+    originalPrice,
+    inStock,
+    img,
+    category
+  };
+  if (!processedIds.has(result.itemId)) {
+    pushList.push(
+      // push data to dataset to be ready for upload to Keboola
+      Apify.pushData(result),
+      // upload JSON+LD data to CDN
+      uploadToS3(
+        s3,
+        `obi${country === "it" ? "-italia" : ""}.${country}`,
+        result.itemId,
+        "jsonld",
+        toProduct(result, {})
+      )
+    );
+    processedIds.add(result.itemId);
+    stats.items += 1;
+  } else {
+    stats.itemsDuplicity += 1;
+  }
+  stats.totalItems += 1;
+  if (pushList.length > 90) {
+    await Promise.all(pushList);
+    pushList = [];
+  }
+}
+
+function parsePrice(text) {
+  let price = text
+    .trim()
+    .replace(/\s|'/g, "")
+    .replace(/,/, ".")
+    // .replace(/'/, "")
+    .match(/(\d+(.\d+)?)/)[0];
+  price = parseFloat(price);
+  return price;
+}
 
 Apify.main(async () => {
   log.info("Actor starts.");
@@ -206,24 +287,26 @@ Apify.main(async () => {
 
   const input = await Apify.getInput();
 
-  const {
-    country = COUNTRY.CZ,
-    development = false,
-    debug = false
-  } = input ?? {};
+  const { development = false, debug = false } = input ?? {};
+  const country =
+    (input && input.country && input.country.toLowerCase()) || "cz";
   global.inputData = { country, development, debug };
 
   if (development || debug) {
     log.setLevel(Apify.utils.log.LEVELS.DEBUG);
   }
 
+  stats = (await Apify.getValue("STATS")) || {
+    urls: 0,
+    items: 0,
+    itemsDuplicity: 0,
+    totalItems: 0
+  };
+
   const requestQueue = await Apify.openRequestQueue();
-  let homePageUrl;
-  if (country === "IT") {
-    homePageUrl = "https://www.obi-italia.it";
-  } else {
-    homePageUrl = `https://www.obi.${country.toLowerCase()}`;
-  }
+  let homePageUrl = `https://www.obi${
+    country === "it" ? "-italia" : ""
+  }.${country}`;
   global.homePageUrl = homePageUrl;
   await requestQueue.addRequest({
     url: homePageUrl,
@@ -233,7 +316,14 @@ Apify.main(async () => {
   });
 
   // await requestQueue.addRequest({
-  //   url: "https://www.obi.cz/vyrovnavaci-hmoty/cemix-beton-b25-25-kg/p/5811302",
+  //   // url: "https://www.obi.de/lounge-gartenmoebel/lounge-set-3-teilig-aus-polyrattan-geflecht-hellgrau/p/1512136",
+  //   // url: "https://www.obi.ru/napolnye-unitazy/napolnyi-unitaz-kompakt-damixa-palace-bit-s-sidenem-mikrolift/p/4811477",
+  //   // url: "https://www.obi-italia.it/faretti/obi-barra-a-4-faretti-nicosia/p/4716742",
+  //   // url: "https://www.obi.pl/dywany-z-krotkim-runem/multidecor-dywan-chindi-50-cm-x-70-cm-mix/p/3147824",
+  //   // url: "https://www.obi.ch/e-bikes/prophete-e-mountainbike-graveler-aeg-sportdrive-27-5-/p/5361118",
+  //   // url: "https://www.obi.at/elektro-pumpen-hauswasserwerke/gardena-pumpe-hauswasserwerk-3700-4/p/8605347#/",
+  //   // url: "https://www.obi.at/gasgrills/char-broil-gasgrill-all-star-120-b/p/8333437#/",
+  //   url: "https://www.obi.sk/lehatka/acamp-zavesne-kreslo-tonga/p/1447697",
   //   userData: {
   //     label: "DETAIL"
   //   }
@@ -243,12 +333,10 @@ Apify.main(async () => {
     useApifyProxy: false
   });
 
-  let pushList = [];
-  const processedIds = new Set();
   const crawler = new Apify.CheerioCrawler({
     requestQueue,
     proxyConfiguration,
-    maxConcurrency: 1,
+    maxConcurrency: 5,
     handlePageFunction: async context => {
       const { label } = context.request.userData;
       context.requestQueue = requestQueue;
@@ -259,56 +347,7 @@ Apify.main(async () => {
       } else if (label === "LIST") {
         await handleList(context);
       } else if (label === "DETAIL") {
-        const { request, $ } = context;
-        log.debug(`[handleDetail] label: ${label} ${request.url}`);
-
-        const itemName = $(".overview__description >.overview__heading").text();
-        const itemId = $('input[name="code"]').attr("value");
-        // todo currency and currentPrice not good
-        const currency = $('meta[itemprop="priceCurrency"]').attr("content");
-        const currentPrice = parseFloat($(".overview__price").text());
-        const discounted = false;
-        const inStock = $(".overview__flag-available").get().length != 0;
-        const img = `https:${$(".ads-slider__link").attr("href")}`;
-        const category = $("li.breadcrumb__dropdown__wrapper > a")
-          .map(function () {
-            return $(this).text();
-          })
-          .get()
-          .join("/");
-        const result = {
-          itemName,
-          itemId,
-          currency,
-          currentPrice,
-          discounted,
-          originalPrice: currentPrice,
-          inStock,
-          img,
-          category,
-          itemUrl: request.url
-        };
-        if (!processedIds.has(result.itemId)) {
-          pushList.push(
-            // push data to dataset to be ready for upload to Keboola
-            Apify.pushData(result),
-            // upload JSON+LD data to CDN
-            uploadToS3(
-              s3,
-              `obi${
-                country === "IT" ? "-italia" : ""
-              }.${country.toLowerCase()}`,
-              result.itemId,
-              "jsonld",
-              toProduct(result, {})
-            )
-          );
-          processedIds.add(result.itemId);
-        }
-        if (pushList.length > 90) {
-          await Promise.all(pushList);
-          pushList = [];
-        }
+        await handleDetail(context, s3, country);
       }
     },
     handleFailedRequestFunction: async ({ request }) => {
@@ -320,16 +359,16 @@ Apify.main(async () => {
   await crawler.run();
   log.info("crawler finished");
 
-  await invalidateCDN(
-    cloudfront,
-    "EQYSHWUECAQC9",
-    `obi.${country.toLowerCase()}`
-  );
-  log.info("invalidated Data CDN");
+  await Apify.setValue("STATS", stats);
+  log.info(JSON.stringify(stats));
+
+  const directoryName = `obi${country === "it" ? "-italia" : ""}.${country}`;
+  await invalidateCDN(cloudfront, "EQYSHWUECAQC9", directoryName);
+  log.info(`invalidated Data CDN ${directoryName}`);
   if (!development) {
-    const tableName = `obi_${country.toLowerCase()}`;
+    const tableName = `obi_${country}`;
     await uploadToKeboola(tableName);
-    log.info("update to Keboola finished.");
+    log.info(`update to Keboola finished ${tableName}.`);
   }
   log.info("Actor Finished.");
 });
