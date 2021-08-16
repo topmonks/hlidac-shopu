022204128cf985dcda83337c212a961bb94f4cf2
Marketa Sauerova <market.sauer@gmail.com>
2021-08-16 16:14:37 +0200

    Megaknihy daily actor

---
+++ actors/megaknihy-daily/src/main.js
@@ -0,0 +1,88 @@
+const Apify = require('apify');
+const cheerio = require('cheerio');
+const UserAgent = require('user-agents');
+const { handleStart, handlePage } = require('./routes');
+
+const { utils: { log } } = Apify;
+
+Apify.main(async () => {
+    const source = {
+        url: 'https://www.megaknihy.cz/',
+        userData: {
+            label: 'START',
+        },
+    };
+    const requestQueue = await Apify.openRequestQueue();
+    await requestQueue.addRequest(source);
+
+    let count = 0;
+    const alreadyScrapedProducts = (await Apify.getValue('ALREADY-SCRAPED')) || [];
+
+    // Set state persistence
+    const persistState = async () => {
+        await Apify.setValue('ALREADY-SCRAPED', alreadyScrapedProducts);
+        log.info(`[PERSIST]: -- Product cache has ${alreadyScrapedProducts.length} unique products.`);
+    };
+
+    Apify.events.on('persistState', persistState);
+
+    // Persist unique product's ID on migrating
+    Apify.events.on('migrating', async () => {
+        log.info('migrating...');
+        await Apify.setValue('ALREADY-SCRAPED', alreadyScrapedProducts);
+    });
+
+    const proxyConfiguration = await Apify.createProxyConfiguration({
+        groups: ['CZECH_LUMINATI'],
+    });
+
+    const crawler = new Apify.PuppeteerCrawler({
+        requestQueue,
+        proxyConfiguration,
+        maxRequestRetries: 10,
+        launchContext: {
+            useChrome: true,
+            stealth: true,
+            // Rotate user agents
+            userAgent: new UserAgent([/Chrome/, { deviceCategory: 'desktop' }]).toString(),
+            launchOptions: {
+                headless: true,
+            },
+        },
+        useSessionPool: true,
+        browserPoolOptions: {
+            retireBrowserAfterPageCount: 30,
+        },
+        maxConcurrency: 1,
+        handlePageTimeoutSecs: 180,
+        handlePageFunction: async (context) => {
+            ++count;
+            const { userData: { label, categories }, url } = context.request;
+            // Set up cheerio properties
+            const body = await context.page.evaluate(() => document.documentElement.innerHTML);
+            const $ = cheerio.load(body);
+            const args = { url, $, body };
+
+            // we are getting blocked by google re-captcha, so the session got useless
+            const captchaButton = $('.g-recaptcha.survey-submit');
+            if (captchaButton.length !== 0) {
+                log.info(`[PAGE-BLOCKED]: Page ${url} was blocked, we need to retry with different session.`);
+                context.session.retire();
+                throw new Error(`${url} was blocked by google re-captcha.`);
+            }
+
+            switch (label) {
+                case 'START':
+                    return handleStart(args, requestQueue);
+                case 'PAGE':
+                    return handlePage(args, categories, requestQueue, context.page, alreadyScrapedProducts);
+                default:
+                    throw new Error(`No route for label: ${label}`);
+            }
+        },
+    });
+
+    log.info('Starting the crawl.');
+    await crawler.run();
+    log.info('Crawl finished.');
+});
---
+++ actors/megaknihy-daily/src/utils.js
@@ -0,0 +1,90 @@
+exports.getAllSubcategories = ($) => {
+    // left panel contains links to all categories with subcategories
+    // we're enqueueing all subcategories to have smaller pagination
+    const subcategories = [];
+    const menu = $("div[id='categories_block_left']").find("ul[class*='tree']").eq(0);
+    $(menu).find("> li:not([class*='action'])")
+        .each((index, category) => {
+            const categoryName = $(category).find('a').eq(0).attr('title');
+            if ($(category).has('ul').length !== 0) {
+                $(category).find('li')
+                    .each((index2, subcategory) => {
+                        subcategories.push({
+                            name: [categoryName, $(subcategory).find('a').eq(0).attr('title')],
+                            url: $(subcategory).find('a').eq(0).attr('href'),
+                        });
+                    });
+            } else {
+                subcategories.push({
+                    name: [categoryName],
+                    url: $(category).find('a').eq(0).attr('href'),
+                });
+            }
+        });
+    return subcategories;
+};
+
+exports.getMaxPaginationNumber = ($) => $("li[id='pagination_next']").prev().eq(0).text();
+
+const modelProductData = ($, product, categories) => {
+    const title = $(product).find('h2 a');
+    const bottomBox = $(product).find("div[class='product-bottom-box']");
+
+    const productData = {
+        itemId: $(title).attr('href').match(/\/([0-9]){4}[0-9]*-/)[0].match(/[0-9]+/)[0],
+        itemImg: $(product).find('img').eq(0).attr('src'),
+        itemUrl: $(title).attr('href'),
+        itemName: $(title).text(),
+        currentPrice: parseInt($(bottomBox).find("span[class='price']").eq(0).text()
+            .match(/[0-9]+/)[0], 10),
+        originalPrice: $(bottomBox).find("span[class='old-price']").eq(0).text(),
+        rating: $(bottomBox).find("span[class='rating']")
+            .eq(0).text().trim(),
+        discounted: false,
+        sale: $(product).find("div[class*='product-sale']").eq(0).text(),
+        category: categories,
+        currency: $(bottomBox).find("span[class='price_currency']").eq(0).text() === 'Kč'
+            ? 'CZK' : $(bottomBox).find("span[class='price_currency']").eq(0).text(),
+        inStock: $(product).find("span[class='product-available']").length > 0,
+    };
+    return productData;
+};
+
+exports.getProductsData = ($, categories, alreadyScrapedProducts) => {
+    const productsData = [];
+    let outOfStockProductDiscovered = false;
+    const products = $("ul[id='product_list']").find('li');
+    products.each((index, product) => {
+        const productData = modelProductData($, product, categories);
+        if (productData.originalPrice) {
+            productData.originalPrice = parseInt(productData.originalPrice.match(/[0-9]+/)[0], 10);
+        } else {
+            // If current price is equal to original price, then original should be null
+            // If the original price is extracted then prices differ, if not they are equal
+            productData.originalPrice = null;
+        }
+        if (!productData.sale) {
+            productData.sale = 0;
+        } else {
+            productData.sale = parseInt(productData.sale.match(/[0-9]+/)[0], 10);
+        }
+        if (productData.rating) {
+            productData.rating = parseFloat(productData.rating.match(/[0-9]+\.?[0-9]*/)[0]);
+        }
+        productData.discounted = !!productData.originalPrice;
+        // Push product data only if it wasn't already scraped - deduplication of products
+        // (on the page there are same products in multiple categories)
+        if (!alreadyScrapedProducts.includes(productData.itemId)) {
+            if (productData.inStock) {
+                productsData.push(productData);
+                alreadyScrapedProducts.push(productData.itemId);
+            } else {
+                outOfStockProductDiscovered = true;
+            }
+        }
+    });
+    return {
+        productsData,
+        outOfStockProductDiscovered,
+    };
+};
