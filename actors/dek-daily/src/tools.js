4d1723c44fac81c45bcd2e54acebfe410bfe005a
lukas.sirhal <e1Sil3487>
2021-12-02 11:55:19 +0100

    [actors] - fix lint

--- actors/dek-daily/src/tools.js
+++ actors/dek-daily/src/tools.js
@@ -1,26 +1,26 @@
-const {COUNTRY, BF} = require("./const");
+const { COUNTRY, BF } = require("./const");
 
 function parsePrice(text) {
-    return parseFloat(
-        text
-            .replace(/\s/g, "")
-            .replace("Kč", "")
-            .replace("€", "")
-            .replace(",", ".")
-            .trim()
-    );
+  return parseFloat(
+    text
+      .replace(/\s/g, "")
+      .replace("Kč", "")
+      .replace("€", "")
+      .replace(",", ".")
+      .trim()
+  );
 }
 
 function enqueueCategories(cats) {
-    let catUrls = [];
-    for (const c of cats) {
-        if (c.category && c.category.length > 0) {
-            catUrls = catUrls.concat(enqueueCategories(c.category));
-        } else {
-            catUrls.push(c.url);
-        }
+  let catUrls = [];
+  for (const c of cats) {
+    if (c.category && c.category.length > 0) {
+      catUrls = catUrls.concat(enqueueCategories(c.category));
+    } else {
+      catUrls.push(c.url);
     }
-    return catUrls;
+  }
+  return catUrls;
 }
 
 /**
@@ -28,8 +28,8 @@ function enqueueCategories(cats) {
  * @return {string}
  */
 function getRootUrl() {
-    const {country = COUNTRY.CZ} = global.userInput;
-    return `https://www.dek.${country.toLowerCase()}`;
+  const { country = COUNTRY.CZ } = global.userInput;
+  return `https://www.dek.${country.toLowerCase()}`;
 }
 
 /**
@@ -37,17 +37,17 @@ function getRootUrl() {
  * @return {string|string}
  */
 function getTableName() {
-    const {type, country = COUNTRY.CZ} = global.userInput;
-    let tableName = `dek${country.toLowerCase()}`;
-    if (type === BF) {
-        tableName = `${tableName}_bf`;
-    }
-    return tableName;
+  const { type, country = COUNTRY.CZ } = global.userInput;
+  let tableName = `dek${country.toLowerCase()}`;
+  if (type === BF) {
+    tableName = `${tableName}_bf`;
+  }
+  return tableName;
 }
 
 module.exports = {
-    getRootUrl,
-    getTableName,
-    enqueueCategories,
-    parsePrice
+  getRootUrl,
+  getTableName,
+  enqueueCategories,
+  parsePrice
 };
