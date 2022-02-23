7450dd5458d28d1bc76f80524f8cc26f28e7e2d0
Jan Fiedler <janfiedler@gmail.com>
2022-02-24 00:56:52 +0100

    [@hlidac-shopu/actors-commo] - Added function cleanPriceText and cleanUnitPriceText - new version release - bump version dependecies

--- actors/common/product.js
+++ actors/common/product.js
@@ -115,6 +115,22 @@ function currencyToISO4217(currency) {
   }
 }
 
+function cleanPriceText(priceText) {
+  priceText = priceText.replace(/\s+/g, "");
+  if (priceText.includes("cca")) priceText = priceText.split("cca")[1];
+  const match = priceText.match(/\d+(:?[,.]\d+)?/);
+  if (!match) return null;
+  return match[0].replace(",", ".");
+}
+
+function cleanUnitPriceText(priceText) {
+  priceText = priceText.replace(/\s+/g, "");
+  if (priceText.includes("/kg")) priceText = priceText.split("/kg")[0];
+  const match = priceText.match(/\d+(:?[,.]\d+)?/);
+  if (!match) return null;
+  return match[0].replace(",", ".");
+}
+
 module.exports = {
   toProduct,
   uploadToS3,
@@ -122,5 +138,7 @@ module.exports = {
   s3FileName,
   shopName,
   invalidateCDN,
-  currencyToISO4217
+  currencyToISO4217,
+  cleanPriceText,
+  cleanUnitPriceText
 };
