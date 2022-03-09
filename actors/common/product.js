773032ebfd7c0085f2c43fde981c7fb548fd6d54
Aleš Roubíček <ales@roubicek.name>
2022-03-09 09:36:31 +0100

    [actors] fix usage of upload to S3 from common

--- actors/common/product.js
+++ actors/common/product.js
@@ -1,6 +1,5 @@
-const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const { PutObjectCommand } = require("@aws-sdk/client-s3");
 const { CreateInvalidationCommand } = require("@aws-sdk/client-cloudfront");
-const s3 = new S3Client({ region: "eu-central-1" });
 /** @typedef { import("@aws-sdk/client-s3").S3Client } S3Client */
 /** @typedef { import("@aws-sdk/client-cloudfront").CloudFrontClient } CloudFrontClient */
 /** @typedef { import("schema-dts").Product} Product */
@@ -52,24 +51,17 @@ async function uploadToS3(s3, shop, fileName, ext, data) {
   );
 }
 
-async function uploadToS3v2(item, { priceCurrency, ...additionalData }) {
-  if (item["itemUrl"] === undefined || item["itemUrl"] === null) {
+async function uploadToS3v2(s3, item, extraData = {}) {
+  if (!item.itemUrl) {
     throw new Error("Item missing attribute itemUrl");
   }
-  let shop = await shopName(item.itemUrl);
-  if (!shop.includes("_")) {
-    shop = await shopName(item.itemUrl, { getFullKey: true });
-  }
-  const fileName = await s3FileName(item);
-  const ext = "jsonld";
-  const data = toProduct(item, { priceCurrency, ...additionalData });
-  await s3.send(
-    new PutObjectCommand({
-      Bucket: "data.hlidacshopu.cz",
-      Key: `products/${shop.replace("_", ".")}/${fileName}.${ext}`,
-      ContentType: `application/${ext}`,
-      Body: JSON.stringify(data)
-    })
+  const { shopOrigin, itemSlug } = await import("@hlidac-shopu/lib/shops.mjs");
+  return uploadToS3(
+    s3,
+    shopOrigin(item.itemUrl),
+    itemSlug(item.itemUrl),
+    "jsonld",
+    toProduct(item, extraData)
   );
 }
 
@@ -90,18 +82,6 @@ async function invalidateCDN(cloudfront, distributionId, shop) {
       }
     })
   );
-}
-
-async function s3FileName(detail) {
-  const { shops, shopName } = await import("@hlidac-shopu/lib/shops.mjs");
-  const url = new URL(detail.itemUrl);
-  const shop = shops.get(shopName(url));
-  return shop.parse(url).itemUrl;
-}
-
-async function shopName(url, options = {}) {
-  const { shopName } = await import("@hlidac-shopu/lib/shops.mjs");
-  return shopName(new URL(url), options);
 }
 
 function currencyToISO4217(currency) {
@@ -135,8 +115,6 @@ module.exports = {
   toProduct,
   uploadToS3,
   uploadToS3v2,
-  s3FileName,
-  shopName,
   invalidateCDN,
   currencyToISO4217,
   cleanPriceText,
