d72e2e172c0be4b33876aa6378529725641e80f4
Jakub Dušek <jakub.dusek@salax.cz>
2022-03-09 21:29:17 +0100

    [hornbach-daily] get rid of "router"

--- actors/hornbach-daily/src/main.js
+++ actors/hornbach-daily/src/main.js
@@ -1,18 +1,107 @@
 import { S3Client } from "@aws-sdk/client-s3";
 import { CloudFrontClient } from "@aws-sdk/client-cloudfront";
 import { uploadToKeboola } from "@hlidac-shopu/actors-common/keboola.js";
-import { invalidateCDN } from "@hlidac-shopu/actors-common/product.js";
+import {
+  invalidateCDN,
+  uploadToS3v2
+} from "@hlidac-shopu/actors-common/product.js";
 import rollbar from "@hlidac-shopu/actors-common/rollbar.js";
 import Apify from "apify";
-import { createRouter } from "./routes.js";
-import { LABELS } from "./const.js";
+import { LABELS, API_URL, PRICE_HEADER } from "./const.js";
+import { siteMapToLinks, getCategoryId, getCategories } from "./tools.js";
 
-const { log } = Apify.utils;
+const { log, requestAsBrowser } = Apify.utils;
+
+async function scrapeSitemapLinks({ body, crawler }, { userInput }) {
+  const { country } = userInput;
+  log.info("START with main page");
+  const links = siteMapToLinks(body);
+  for (const link of links) {
+    const id = getCategoryId(link);
+    await crawler.requestQueue.addRequest(
+      {
+        url: API_URL(country, id),
+        userData: {
+          label: LABELS.CATEGORY,
+          categoryId: id,
+          link
+        }
+      },
+      { forefront: true }
+    );
+  }
+}
+
+async function scrapeCategory({ request, json, crawler }, { userInput, s3 }) {
+  const { country } = userInput;
+  const { pageNumber, pageCount, articles } = json;
+  if (pageNumber === 1) {
+    const {
+      userData: { categoryId }
+    } = request;
+    for (let i = 2; i <= pageCount; i++) {
+      await crawler.requestQueue.addRequest({
+        url: API_URL(country, categoryId, i),
+        userData: {
+          label: LABELS.CATEGORY,
+          categoryId,
+          page: i
+        }
+      });
+    }
+  }
+  if (articles.length > 0) {
+    const requests = [];
+    const codes = articles.map(a => a.articleCode);
+    const { body } = await requestAsBrowser({
+      url: `https://www.hornbach.${country}/mvc/article/displaystates-and-prices.json`,
+      method: "POST",
+      json: true,
+      useHttp2: true,
+      headers: PRICE_HEADER,
+      payload: JSON.stringify(codes)
+    });
+    for (const article of articles) {
+      let currentPrice = article.allPrices.displayPrice.price.replace(",", ".");
+      const result = {
+        itemId: article.articleCode,
+        itemUrl: `https://www.hornbach.${country}${article.localizedExternalArticleLink}`,
+        itemName: article.title,
+        currency: article.allPrices.displayPrice.currency,
+        img: article.imageUrl,
+        currentPrice: parseFloat(currentPrice),
+        originalPrice: null,
+        discounted: false,
+        category: getCategories(article.categoryPath)
+      };
+      const price = body.filter(p => p.articleCode === article.articleCode);
+      if (price) {
+        const { allPrices } = price[0];
+        let { displayPrice, guidingPrice } = allPrices;
+        if (guidingPrice) {
+          displayPrice = displayPrice.price.replace(",", ".");
+          guidingPrice = guidingPrice.price.replace(",", ".");
+          result.currentPrice = parseFloat(displayPrice);
+          result.discounted = true;
+          result.originalPrice = parseFloat(guidingPrice);
+        }
+      }
+      requests.push(
+        Apify.pushData(result),
+        uploadToS3v2(s3, result, {
+          priceCurrency: result.currency,
+          inStock: true
+        })
+      );
+    }
+    await Promise.all(requests);
+  }
+}
 
 Apify.main(async () => {
   rollbar.init();
-  global.userInput = await Apify.getInput();
-  const { country = "cz" } = global.userInput;
+  const userInput = await Apify.getInput();
+  const { country = "cz" } = userInput;
   const requestQueue = await Apify.openRequestQueue();
   if (country === "cz") {
     await requestQueue.addRequest({
@@ -30,16 +119,12 @@ Apify.main(async () => {
     });
   }
 
-  global.s3 = new S3Client({ region: "eu-central-1" });
+  const s3 = new S3Client({ region: "eu-central-1" });
   const cloudfront = new CloudFrontClient({ region: "eu-central-1" });
   const proxyConfiguration = await Apify.createProxyConfiguration({
     groups: ["CZECH_LUMINATI"]
   });
 
-  // Create route
-  const router = createRouter();
-
-  // Set up the crawler, passing a single options object as an argument.
   const crawler = new Apify.CheerioCrawler({
     requestQueue,
     proxyConfiguration,
@@ -51,7 +136,12 @@ Apify.main(async () => {
         userData: { label }
       } = request;
 
-      await router(label, context);
+      switch (label) {
+        case LABELS.CATEGORY:
+          return scrapeCategory(context, { s3, userInput });
+        case LABELS.SITE:
+          return scrapeSitemapLinks(context, { userInput });
+      }
     },
     // If request failed 4 times then this function is executed
     handleFailedRequestFunction: async ({ request }) => {
