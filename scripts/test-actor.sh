4c0ea14a922ab055996a902732b548cd606e6fdd
Dan Hromada <hromada.dan@gmail.com>
2022-03-22 08:09:39 +0100

    Add actor test script

---
+++ scripts/test-actor.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+INPUT_PATH="apify_storage/key_value_stores/default/INPUT.json"
+
+if [[ "$PWD" =~ .*/actors/.* ]]; then
+  if [ ! -e "$file" ] ; then
+    echo '{}' > $INPUT_PATH
+  fi
+  \cp $INPUT_PATH $INPUT_PATH.tmp
+  json="$(jq --arg type TEST '. + {type: $type}' $INPUT_PATH)"
+  echo $json > $INPUT_PATH
+  TEST=1 apify run -p
+  \mv $INPUT_PATH.tmp $INPUT_PATH
+fi
