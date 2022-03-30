56f77d58c1e7d35113c925b882279287a9e2576c
Dan Hromada <hromada.dan@gmail.com>
2022-03-30 18:29:28 +0200

    [megapixel] Fix widget on out of stock products

--- extension/shops/megapixel.mjs
+++ extension/shops/megapixel.mjs
@@ -9,8 +9,9 @@ export class Megapixel extends Shop {
   async scrape() {
     const elem = document.querySelector("div#snippet--price");
     if (!elem) return;
-    const itemIdUrl = document.querySelector("p.sticky-bar-product__buttons > a").href;
-    const url = new URL(itemIdUrl);
+    const linkElement = document.querySelector("a.service-list__link");
+    if (!linkElement) return;
+    const url = new URL(linkElement.href);
     const itemId = url.searchParams.get("produkt");
     const title = document.querySelector("h1").innerText.trim();
     const currentPrice = cleanPrice(".product-detail__price-vat");
