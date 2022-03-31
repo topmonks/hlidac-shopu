c75ba359e9dcfbf0aaaa05ca2da9d82a3b53aae5
Dan Hromada <hromada.dan@gmail.com>
2022-03-31 12:56:16 +0200

    [iglobus-daily] One actor, one shop, one file

--- actors/iglobus-daily/main.js
+++ actors/iglobus-daily/main.js
@@ -3,9 +3,21 @@ import { uploadToKeboola } from "@hlidac-shopu/actors-common/keboola.js";
 import { invalidateCDN } from "@hlidac-shopu/actors-common/product.js";
 import rollbar from "@hlidac-shopu/actors-common/rollbar.js";
 import Apify from "apify";
-import { handleStart, handleList } from "./routes.js";
+import { S3Client } from "@aws-sdk/client-s3";
+import {
+  cleanPriceText,
+  cleanUnitPriceText,
+  uploadToS3v2
+} from "@hlidac-shopu/actors-common/product.js";
+import { URL } from "url";
 
 const { log } = Apify.utils;
+const s3 = new S3Client({ region: "eu-central-1" });
+
+const HOST = "https://www.iglobus.cz";
+const listUrl = x => `${HOST}${x}/core?razeni=cena&strana=0`;
+const productUrl = x => `${HOST}${x}`;
+const canonicalUrl = x => new URL(x, HOST);
 
 Apify.main(async () => {
   rollbar.init();
@@ -77,3 +89,107 @@ Apify.main(async () => {
   }
   log.info("Finished.");
 });
+
+async function handleStart({ request, $ }, crawlContext) {
+  const { requestQueue, stats } = crawlContext;
+  const listLinks = $("#categories a")
+    .map(function () {
+      return $(this).attr("href");
+    })
+    .get();
+  for (const link of listLinks) {
+    await requestQueue.addRequest({
+      url: listUrl(link),
+      userData: {
+        label: "LIST",
+        page: 0,
+        category: link.replace("/", "").replaceAll("-", " ")
+      }
+    });
+  }
+  stats.categories = listLinks.length;
+  log.info(`Found ${listLinks.length}x categories`);
+}
+
+async function handleList({ request, $ }, crawlContext) {
+  // Handle pagination
+  const { url, userData } = request;
+  const { requestQueue, processedIds, stats } = crawlContext;
+  stats.pages++;
+  if (userData.page === 0) {
+    const paginationLinks = $(".pagination .page-link")
+      .map(function () {
+        return parseInt($(this).text().trim());
+      })
+      .get();
+    if (paginationLinks.length > 0) {
+      //get last pagination page
+      const lastPaginationPage = paginationLinks[paginationLinks.length - 2];
+      for (let i = 1; i < lastPaginationPage; i++) {
+        const newUrl = canonicalUrl(url);
+        newUrl.searchParams.set("strana", i.toString());
+        userData.page = i;
+        await requestQueue.addRequest(
+          {
+            url: newUrl.href,
+            userData
+          },
+          { forefront: true }
+        );
+      }
+    }
+  }
+  // Handle products on list
+  const $products = $(".products article.product-wrap");
+  try {
+    const products = await extractItems($, $products, userData.category);
+    // we don't need to block pushes, we will await them all at the end
+    const requests = [];
+    for (const product of products) {
+      if (!processedIds.has(product.itemId)) {
+        processedIds.add(product.itemId);
+        requests.push(Apify.pushData(product), uploadToS3v2(s3, product));
+        stats.items++;
+      } else {
+        stats.itemsDuplicity++;
+      }
+    }
+    log.info(`Found ${requests.length / 2} unique products`);
+    // await all requests, so we don't end before they end
+    await Promise.allSettled(requests);
+  } catch (e) {
+    console.error(e);
+    console.log(`Failed extraction of items. ${request.url}`);
+  }
+}
+
+async function extractItems($, $products, category) {
+  const itemsArray = [];
+  $products.each(function () {
+    const result = {};
+    const $head = $(this).find(".article-head");
+    const $product = $(this).find(".product-box");
+
+    result.itemId = $(this).attr("data-stock-item-code");
+    result.itemName = $head.attr("data-title");
+    result.itemUrl = productUrl(
+      $head.find(".product-detail-link").attr("href")
+    );
+    result.img = $head.find(".img-fluid").eq(0).attr("src");
+    const currentPrice = $product.find(".product-price").text().trim();
+    result.currentPrice = cleanPriceText(currentPrice);
+    result.originalPrice = cleanPriceText(
+      $product.find(".product-price-before").text().trim()
+    );
+    result.currentUnitPrice = cleanUnitPriceText(
+      $product.find(".product-price-unit").text().trim()
+    );
+    result.useUnitPrice = currentPrice.includes("cca");
+    result.discounted = result.currentPrice < result.originalPrice;
+    result.currency = "CZK";
+    result.instStock = $head.attr("data-availability") === "skladem";
+    result.category = category;
+    itemsArray.push(result);
+  });
+  return itemsArray;
+}
