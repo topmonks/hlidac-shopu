a7d63320932ce6175097de09ab14fe9785bbf8c8
Daniel Hromada <hromada.dan@gmail.com>
2022-06-13 14:13:02 +0200

    [hornbach-daily] browse categories
    this is WIP actor

--- actors/hornbach-daily/main.js
+++ actors/hornbach-daily/main.js
@@ -15,34 +15,129 @@ const COUNTRY = {
 const LABELS = {
   TOP_CATEGORIES: "TOP_CATEGORIES",
   SUB_CATEGORIES: "SUB_CATEGORIES",
-  CATEGORY_PRODUCTS: "CATEGORY_PRODUCTS"
+  CAT_PRODUCTS: "CAT_PRODUCTS"
 };
 
-const { BasicCrawler, getInput, main, openRequestQueue, pushData } = Apify;
+const {
+  BasicCrawler,
+  getInput,
+  main,
+  openRequestQueue,
+  pushData,
+} = Apify;
+
+const completeUrl = (country, path) => (
+  `https://www.hornbach.${country.toLowerCase()}${path}`
+);
+
+async function scrapeTopCategories({ document, requestQueue, country }) {
+  const links = document.querySelectorAll(
+    `[data-testid="product-category"] h2 a`
+  );
+  for (let link of links) {
+    const href = link.getAttribute("href");
+    await requestQueue.addRequest({
+      url: completeUrl(country, href),
+      userData: {
+        label: LABELS.SUB_CATEGORIES,
+      }
+    });
+  }
+}
+
+async function scrapeSubCategories({
+  document,
+  requestQueue,
+  country,
+  url,
+  userData,
+}) {
+  const links = document.querySelectorAll(
+    `[data-testid="categories-rondell-card"] a`
+  );
+  const { innerText: categoryTitle } = document.querySelector(
+    `[data-testid="category-page-header"] h1`
+  );
+
+  const category = userData.category
+    ? `${userData.category} > ${categoryTitle}`
+    : categoryTitle;
+
+  if (links.length) {
+    for (let link of links) {
+      const href = link.getAttribute("href");
+      await requestQueue.addRequest({
+        url: completeUrl(country, href),
+        userData: {
+          label: LABELS.SUB_CATEGORIES,
+          category,
+        }
+      });
+    }
+  } else {
+    await requestQueue.addRequest({
+      uniqueKey: category,
+      url,
+      userData: {
+        label: LABELS.CAT_PRODUCTS,
+        category,
+      }
+    });
+  }
+}
+
+async function scrapeCatProducts({
+  document,
+  requestQueue,
+  country,
+  url,
+  userData,
+}) {
+  const { category } = userData;
+  console.warn("product scraper not yet implemented", { url, category });
+}
 
 main(async () => {
-  const { type = ActorType.FULL, country } = (await getInput()) ?? {};
-  const urlBase = `https://www.hornbach.${country.toLowerCase()}/c/`;
+  const {
+    type = ActorType.FULL,
+    country = COUNTRY.CZ,
+  } = (await getInput()) ?? {};
 
   const requestQueue = await openRequestQueue();
 
-  await requestQueue.addRequest(urlBase, {
+  await requestQueue.addRequest({
+    url: completeUrl(country, "/c/"),
     userData: {
-      label: LABELS.TOP_CATEGORIES
-    }
+      label: LABELS.TOP_CATEGORIES,
+    },
   });
 
   const crawler = new BasicCrawler({
     requestQueue,
     async handleRequestFunction({ request }) {
-      const { url } = request;
+      const { url, userData } = request;
       const resp = await fetch(url);
       const body = await resp.text();
       const { document } = parseHTML(body);
 
-      await pushData({
-        noop: 1
-      });
+      switch (userData.label) {
+
+        case LABELS.TOP_CATEGORIES:
+          await scrapeTopCategories({ document, requestQueue, country });
+          break;
+
+        case LABELS.SUB_CATEGORIES:
+          await scrapeSubCategories({ document, requestQueue, country, url, userData });
+          break;
+
+        case LABELS.CAT_PRODUCTS:
+          await scrapeCatProducts({ document, requestQueue, country, url, userData });
+          break;
+      }
+
+      // await pushData({
+      //   noop: 1
+      // });
     }
   });
 
