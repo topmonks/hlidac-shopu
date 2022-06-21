d3a903ca5d978d2a77a44a21fcf4eb487b54491b
Aleš Roubíček <ales@roubicek.name>
2022-06-21 15:13:28 +0200

    [actors] Actor should wait till uploader is done

--- actors/common/keboola.js
+++ actors/common/keboola.js
@@ -15,18 +15,12 @@ export async function uploadToKeboola(tableName) {
   /** @type {ApifyEnv} */
   const env = await Apify.getEnv();
   /** @type {ActorRun} */
-  const run = await Apify.call(
-    "blackfriday/uploader",
-    {
-      datasetId: env.defaultDatasetId,
-      upload: true,
-      actRunId: env.actorRunId,
-      tableName
-    },
-    {
-      waitSecs: 60
-    }
-  );
+  const run = await Apify.call("blackfriday/uploader", {
+    datasetId: env.defaultDatasetId,
+    upload: true,
+    actRunId: env.actorRunId,
+    tableName
+  });
 
   log.info(`Keboola upload called: ${run.id}`, { run });
 }
