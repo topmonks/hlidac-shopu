3c9d8031c0609b10f62563579fb6a2aec591b6b3
Aleš Roubíček <ales@roubicek.name>
2022-07-25 09:50:54 +0200

    [www.hlidacshopu.cz] Added new media mention

--- scripts/media-generator.mjs
+++ scripts/media-generator.mjs
@@ -8,7 +8,7 @@ import { URL } from "url";
 
 const test = false;
 const urls = [
-  "https://tech.hn.cz/c1-67092220-nakupni-svatek-na-48-hodin-nemecky-prime-day-proti-brutalnimu-vyprodeji-a-cenovym-bombam"
+  "https://www.zive.cz/clanky/vy-a-pocitac-nakupujete-na-internetu-podle-srovnavacu-cen/sc-3-a-217487/default.aspx"
 ];
 
 const template = ({ url, title, date, perex, filename }) => `---
