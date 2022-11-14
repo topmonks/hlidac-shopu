d9e24edf2e0115133987e2e2919b4fede069a385
Aleš Roubíček <ales@roubicek.name>
2022-11-14 17:23:40 +0100

    [actors/uploader] Fix broken parts

--- actors/uploader/src/uploader.js
+++ actors/uploader/src/uploader.js
@@ -1,17 +1,18 @@
-import { fetch } from "fetch-h2";
-import Apify from "apify";
+import { sleep } from "@crawlee/utils";
+import { Actor, log } from "apify";
 import byteSize from "byte-size";
+import { fetch } from "fetch-h2";
 import FormData from "form-data";
 
 const { KEBOOLA_URI, KEBOOLA_KEY } = process.env;
 
 /**
  *
- * @param bucket
- * @param table
+ * @param {string} bucket
+ * @param {string} table
  * @param {Buffer} data
- * @param fileName
- * @param isGzipped
+ * @param {string}fileName
+ * @param {boolean} isGzipped
  * @returns {Promise<void>}
  */
 export async function keboolaUploader(
@@ -41,23 +42,23 @@ export async function keboolaUploader(
         headers: body.getHeaders({ "X-StorageApi-Token": KEBOOLA_KEY }),
         body
       });
-      console.log(`HTTP ${resp.status}`);
+      log.info(`HTTP ${resp.status}`);
       if (!resp.ok) {
         const { message } = await resp.json();
         throw new Error(message);
       }
       const elapsed = ((Date.now() - start) / 1000).toFixed(2);
       const { value, unit } = byteSize(size);
-      console.log(`Uploaded ${value}${unit} to ${tableId} in ${elapsed}s.`);
+      log.info(`Uploaded ${value}${unit} to ${tableId} in ${elapsed}s.`);
       return;
-    } catch (e) {
-      lastError = e;
-      console.log(
-        `Upload to Keboola failed on ${i + 1} try, will wait awhile...`
+    } catch (err) {
+      lastError = err;
+      log.error(
+        `Upload to Keboola failed on ${i + 1} try, will wait awhile...`,
+        err
       );
-      console.log(e.message);
-      await Apify.utils.sleep((i + 1) * 10 * 1000);
-      await Apify.setValue("debugFile.csv", data, {
+      await sleep((i + 1) * 10 * 1000);
+      await Actor.setValue("debugFile.csv", data, {
         contentType: "text/csv"
       });
     }
