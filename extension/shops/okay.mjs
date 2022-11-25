1b1039c2b62c6e076f6a3481c07e822bbd3c0562
Daniel Hromada <hromada.dan@gmail.com>
2022-11-25 13:17:38 +0100

    [extension] fix prices selectors on Okay

--- extension/shops/okay.mjs
+++ extension/shops/okay.mjs
@@ -11,8 +11,9 @@ export class Okay extends Shop {
     if (!elem) return;
     const itemId = document.querySelector("div.product-gallery__main")
       .attributes["data-product-id"].textContent;
-    const originalPrice = cleanPrice("p.was-price  span.money");
-    const currentPrice = cleanPrice("p.current_price span.money");
+    const originalPrice = cleanPrice("p.current_price span.money");
+    const currentPrice =
+      cleanPrice("p.current_price_mz span.money.sale") || originalPrice;
     const title = elem.querySelector("h1").innerText.trim();
     const imageUrl = document.querySelector(".product-gallery__link").href;
     return { itemId, title, currentPrice, originalPrice, imageUrl };
