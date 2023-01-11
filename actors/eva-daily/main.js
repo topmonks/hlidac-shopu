3320d7723c9c58aa0f7e78218ed0d4799a9adb2b
Jakub Dušek <jakub.dusek@salax.cz>
2023-01-11 12:45:45 +0100

    [actors/eva-daily] upgrade + refactoring

--- actors/eva-daily/main.js
+++ actors/eva-daily/main.js
@@ -5,147 +5,206 @@ import {
   invalidateCDN,
   uploadToS3v2
 } from "@hlidac-shopu/actors-common/product.js";
-import Apify from "apify";
+import { Actor, Dataset, KeyValueStore, log } from "apify";
+import { HttpCrawler } from "@crawlee/http";
 import rollbar from "@hlidac-shopu/actors-common/rollbar.js";
 import { ActorType } from "@hlidac-shopu/actors-common/actor-type.js";
-
-const { log } = Apify.utils;
+import { withPersistedStats } from "@hlidac-shopu/actors-common/stats.js";
+import { parseHTML } from "linkedom/cached";
 
 const HOST = "https://www.eva.cz";
 
-const completeUrl = x => `${HOST}${x}`;
-
-async function enqueueRequests(requestQueue, urls, userData) {
-  for (const url of urls) {
-    await requestQueue.addRequest(
-      {
-        url,
-        userData
-      },
-      { forefront: true }
-    );
-  }
-}
-
-async function handlePagination($, request, requestQueue) {
-  const nextPagination = $("ul.pagination i.fa-angle-right");
+function handlePagination(document, request) {
+  const nextPagination = document.querySelectorAll(
+    "ul.pagination i.fa-angle-right"
+  );
   if (nextPagination.length > 0) {
     const nextFirst = request.userData.first + 36;
     const url = new URL(request.url);
     url.searchParams.set("first", nextFirst.toString());
     url.search = url.searchParams.toString();
-    console.log(`${request.url} Found pagination page ${url.toString()}`);
-    await requestQueue.addRequest(
-      {
-        url: url.toString(),
-        userData: {
-          label: "PAGE",
-          first: nextFirst
-        }
-      },
-      { forefront: true }
-    );
+    log.info(`${request.url} Found pagination page ${url}`);
+    return {
+      url: url.toString(),
+      userData: {
+        label: "PAGE",
+        first: nextFirst
+      }
+    };
   }
 }
 
-function parseItem($, category) {
-  return el => {
-    const $el = $(el);
-    //Check if item is unpacked/used
-    if ($el.find("div.sgnao2").length === 0) {
-      const itemUrl = $el.find("div.pb-1 h2 a");
-      return {
-        itemId: itemUrl.attr("href").match(/\/([^zbozi\/]+)\//)?.[1],
-        itemName: itemUrl.text(),
-        itemUrl: new URL(itemUrl.attr("href"), HOST).href,
-        img: "https:" + $el.find("img").attr("data-src"),
-        currentPrice: $el.find("span.price").text().replace(/\s/g, "").trim(),
-        originalPrice: null,
-        discounted: category.includes("akční nabídka"),
-        currency: "CZK",
-        inStock: $el
-          .find("div.st_onstore")
-          .text()
-          .toLowerCase()
-          .includes("skladem")
-          ? true
-          : $el
-              .find("div.st_onstore2")
-              .text()
-              .toLowerCase()
-              .includes("skladem u dodavatele"),
-        category
-      };
-    }
-  };
+function parseItem(el, category) {
+  // Check if item is unpacked/used
+  if (!el.querySelector("div.sgnao2")) {
+    const itemUrl = el.querySelector("div.pb-1 h2 a");
+    return {
+      itemId: itemUrl.getAttribute("href").match(/\/([^zbozi\/]+)\//)?.[1],
+      itemName: itemUrl.innerText,
+      itemUrl: new URL(itemUrl.getAttribute("href"), HOST).href,
+      img: `https:${el.querySelector("img").getAttribute("data-src")}`,
+      currentPrice: el
+        .querySelector("span.price")
+        .innerText.replace(/\s/g, "")
+        .trim(),
+      originalPrice: null,
+      discounted: category.includes("akční nabídka"),
+      currency: "CZK",
+      inStock: Boolean(
+        el
+          .querySelector("div.st_onstore")
+          ?.innerText?.toLowerCase()
+          ?.includes("skladem") ||
+          el
+            .querySelector("div.st_onstore2")
+            ?.innerText?.toLowerCase()
+            ?.includes("skladem u dodavatele")
+      ),
+      category
+    };
+  }
 }
 
-async function handleItems($, request, processedIds, stats, s3) {
-  const category = $("div#regularcontent h1").text();
-  const products = $("#content_list div.zitembox")
-    .get()
-    .map(parseItem($, category))
-    .filter(function (element) {
-      //remove undefined due to unpacked/used items
-      return element !== undefined;
-    });
+function extractProducts(document) {
+  const category = document.querySelector("div#regularcontent h1")?.innerText;
+  if (!category) return [];
+  const products = document
+    .querySelectorAll("#content_list div.zitembox")
+    .map(el => parseItem(el, category))
+    .filter(Boolean);
+  return products;
+}
 
-  // we don't need to block pushes, we will await them all at the end
-  const requests = [];
+async function saveProducts(s3, products) {
+  const requests = [Dataset.pushData(products)];
   for (const product of products) {
-    if (!processedIds.has(product.itemId)) {
-      processedIds.add(product.itemId);
-      requests.push(Apify.pushData(product), uploadToS3v2(s3, product));
-      stats.items++;
-    } else {
-      stats.itemsDuplicity++;
-    }
+    requests.push(uploadToS3v2(s3, product));
   }
-  console.log(`${request.url} Found ${requests.length / 2} unique products`);
-  // await all requests, so we don't end before they end
   await Promise.all(requests);
 }
 
-Apify.main(async function main() {
+async function main() {
   rollbar.init();
-  let stats = {};
-  const processedIds = new Set();
   const s3 = new S3Client({ region: "eu-central-1", maxAttempts: 3 });
   const cloudfront = new CloudFrontClient({
     region: "eu-central-1",
     maxAttempts: 3
   });
-  const input = await Apify.getInput();
-  const {
-    development = false,
-    debug = false,
-    maxRequestRetries = 3,
-    maxConcurrency = 10,
-    proxyGroups = ["CZECH_LUMINATI"],
-    type = ActorType.FULL
-  } = input ?? {};
 
-  stats = (await Apify.getValue("STATS")) || {
+  const input = (await KeyValueStore.getInput()) || {};
+  const {
+    development = process.env.TEST,
+    maxRequestRetries = 3,
+    proxyGroups = ["CZECH_LUMINATI"],
+    type = ActorType.Full
+  } = input;
+
+  const stats = await withPersistedStats(x => x, {
     categories: 0,
     pages: 0,
-    items: 0,
-    itemsSkipped: 0,
-    itemsDuplicity: 0,
-    failed: 0
-  };
+    items: 0
+  });
 
-  // Get queue and enqueue first url.
-  const requestQueue = await Apify.openRequestQueue();
+  log.info("ACTOR - setUp crawler");
+  const proxyConfiguration = await Actor.createProxyConfiguration({
+    groups: proxyGroups
+    // useApifyProxy: !development
+  });
 
+  const crawler = new HttpCrawler({
+    proxyConfiguration,
+    maxRequestRetries,
+    maxRequestsPerMinute: 300,
+    maxConcurrency: 5, // they have robots detection
+    async requestHandler({ request, body, crawler, log, session }) {
+      const { document } = parseHTML(body.toString());
+      if (request.userData.label === "PAGE") {
+        //Check if there is next pagination
+        await crawler.requestQueue.addRequest(
+          handlePagination(document, request)
+        );
+        const products = extractProducts(document);
+        log.info(`${request.url} Found ${products.length} products`);
+        if (!products.length) session.markBad();
+        stats.inc("pages");
+        await saveProducts(s3, products);
+      } else if (request.userData.label === "CATEGORY") {
+        // Add subcategories if this category has also products
+        const subcategories = document
+          .querySelectorAll("div#mele div.lmsubmenu div.mlevel a")
+          .map(a => ({
+            url: `${HOST}${a.getAttribute("href")}`,
+            userData: {
+              label: "CATEGORY",
+              first: 0
+            }
+          }));
+        if (subcategories.length > 0) {
+          stats.add("categories", subcategories.length);
+          log.info(
+            `${request.url} Found ${subcategories.length} subcategories`
+          );
+          await crawler.requestQueue.addRequests(subcategories, {
+            forefront: true
+          });
+        }
+
+        //Check if there is next pagination
+        const requests = handlePagination(document, request);
+        if (requests) {
+          await crawler.requestQueue.addRequest(requests);
+        }
+        const products = extractProducts(document);
+        log.info(`${request.url} Found ${products.length} products`);
+        if (!products.length) session.markBad();
+        await saveProducts(s3, products);
+      } else if (request.userData.label === "START") {
+        const links = document
+          .querySelectorAll("#mele > div.lmitem > a")
+          .map(a => ({
+            url: `${HOST}${a.getAttribute("href")}`,
+            userData: {
+              label: type === "COUNT" ? "COUNT" : "CATEGORY",
+              first: 0
+            }
+          }))
+          .filter(
+            ({ url }) =>
+              !url.includes("novinky") &&
+              !url.includes("rozbalene") &&
+              !url.includes("vyprodej")
+          );
+        //If type is COUNT, use category urls for count all products
+        stats.add("categories", links.length);
+        log.info(`${request.url} Found ${links.length} categories`);
+        await crawler.requestQueue.addRequests(links, { forefront: true });
+      } else if (request.userData.label === "COUNT") {
+        //Not unique items. Can include hidden items,unpacked, used, duplicity listing
+        const countElement = document.querySelector(
+          "div#content_filter div.fpanel_inside div.float-right"
+        );
+        if (countElement) {
+          const countItems = Number(countElement.innerText);
+          stats.add("items", countItems);
+          log.info(`${request.url} Counted ${countItems} items`);
+        }
+      }
+    },
+    failedRequestHandler({ request }, error) {
+      log.error(`Request ${request.url} failed multiple times`, error);
+    }
+  });
+
+  const startingRequests = [];
   if (type === ActorType.FULL || type === "COUNT") {
-    await requestQueue.addRequest({
+    startingRequests.push({
       url: "https://eva.cz",
       userData: {
         label: "START"
       }
     });
   } else if (type === ActorType.TEST) {
-    await requestQueue.addRequest({
+    startingRequests.push({
       url: `https://www.eva.cz/oddeleni/mraznicky-pultove/`,
       userData: {
         label: "CATEGORY",
@@ -153,105 +212,17 @@ Apify.main(async function main() {
       }
     });
   }
+  await crawler.run(startingRequests);
 
-  log.info("ACTOR - setUp crawler");
-  /** @type {ProxyConfiguration} */
-  const proxyConfiguration = await Apify.createProxyConfiguration({
-    groups: proxyGroups,
-    useApifyProxy: !development
-  });
+  log.info("crawler finished");
 
-  const crawler = new Apify.CheerioCrawler({
-    requestQueue,
-    proxyConfiguration,
-    maxRequestRetries,
-    maxConcurrency,
-    handlePageFunction: async ({ request, $ }) => {
-      if (request.userData.label === "PAGE") {
-        //Check if there is next pagination
-        await handlePagination($, request, requestQueue);
-        //Scrap products from page
-        await handleItems($, request, processedIds, stats, s3);
-        stats.pages++;
-      } else if (request.userData.label === "CATEGORY") {
-        try {
-          // Add subcategories if this category has also products
-          const subcategories = $("div#mele div.lmsubmenu div.mlevel")
-            .last()
-            .find("a")
-            .map(function () {
-              return completeUrl($(this).attr("href"));
-            })
-            .get();
-          if (subcategories.length > 0) {
-            stats.categories += subcategories.length;
-            console.log(
-              `${request.url} Found ${subcategories.length} subcategories`
-            );
-            await enqueueRequests(requestQueue, subcategories, {
-              label: "CATEGORY",
-              first: 0
-            });
-          }
-
-          //Check if there is next pagination
-          await handlePagination($, request, requestQueue);
-          //Scrap products from page
-          await handleItems($, request, processedIds, stats, s3);
-        } catch (e) {
-          console.log(`Error processing url ${request.url}`);
-          console.error(e);
-        }
-      } else if (request.userData.label === "START") {
-        const links = $("#mele > div.lmitem > a")
-          .map(function () {
-            return completeUrl($(this).attr("href"));
-          })
-          .get()
-          .filter(
-            x =>
-              !x.includes("novinky") &&
-              !x.includes("rozbalene") &&
-              !x.includes("vyprodej")
-          );
-        //If type is COUNT, use category urls for count all products
-        await enqueueRequests(requestQueue, links, {
-          label: type === "COUNT" ? "COUNT" : "CATEGORY",
-          first: 0
-        });
-        stats.categories += links.length;
-        console.log(`${request.url} Found ${links.length} categories`);
-      } else if (request.userData.label === "COUNT") {
-        //Not unique items. Can include hidden items,unpacked, used, duplicity listing
-        const countElement = $(
-          "div#content_filter div.fpanel_inside div.float-right"
-        );
-        if (countElement.length > 0) {
-          const countItems = parseInt(countElement.text(), 10);
-          stats.items += countItems;
-          console.log(`${request.url} Counted ${countItems} items`);
-        }
-      }
-    },
-
-    // If request failed 4 times then this function is executed.
-    handleFailedRequestFunction: async ({ request }) => {
-      console.log(`Request ${request.url} failed multiple times`);
-    }
-  });
-
-  // Run crawler.
-  await crawler.run();
-
-  console.log("crawler finished");
-
-  await Apify.setValue("STATS", stats).then(() => log.debug("STATS saved!"));
-  log.info(JSON.stringify(stats));
-
+  stats.save(true);
   if (!development && type !== "COUNT") {
     await invalidateCDN(cloudfront, "EQYSHWUECAQC9", "eva.cz");
     log.info("invalidated Data CDN");
     await uploadToKeboola("eva_cz");
     log.info("upload to Keboola finished");
   }
-});
+}
+
+await Actor.main(main);
