9ec288dd577b1ef787afb9ddcecf66f48265a12a
Jakub Dušek <jakub.dusek@salax.cz>
2023-01-12 16:27:38 +0100

    [actors/iglobus-daily] upgrade + refactoring

--- actors/iglobus-daily/main.js
+++ actors/iglobus-daily/main.js
@@ -2,7 +2,8 @@ import { CloudFrontClient } from "@aws-sdk/client-cloudfront";
 import { uploadToKeboola } from "@hlidac-shopu/actors-common/keboola.js";
 import { invalidateCDN } from "@hlidac-shopu/actors-common/product.js";
 import rollbar from "@hlidac-shopu/actors-common/rollbar.js";
-import Apify from "apify";
+import { Actor, Dataset, KeyValueStore, log, LogLevel } from "apify";
+import { HttpCrawler } from "@crawlee/http";
 import { S3Client } from "@aws-sdk/client-s3";
 import {
   cleanPriceText,
@@ -10,121 +11,244 @@ import {
   uploadToS3v2
 } from "@hlidac-shopu/actors-common/product.js";
 import { URL } from "url";
+import { ActorType } from "@hlidac-shopu/actors-common/actor-type.js";
+import { parseHTML } from "linkedom/cached";
+import { withPersistedStats } from "@hlidac-shopu/actors-common/stats.js";
 
-const { log } = Apify.utils;
+const rootUrl = "https://shop.iglobus.cz";
 
-const ROOT_URL = "https://shop.iglobus.cz";
-const LABELS = {
+/** @enum {string} */
+const Labels = {
   START: "START",
   LIST: "LIST",
   COUNT: "COUNT"
 };
-// create object TYPE cofied from LABERLS
-const TYPE = {
-  FULL: "FULL",
-  TEST: "TEST",
-  COUNT: "COUNT"
-};
-const STORES = {
+
+/** @enum {string} */
+const Stores = {
   ZLI: "ZLI",
   OST: "OST"
 };
 
-const rootUrl = ({ store = STORES.ZLI } = {}) =>
-  `${ROOT_URL}/store/switch?store=${store}&referer-url=/cs/outlet`;
+function extractItems(document, category) {
+  return document
+    .querySelectorAll(".product-list product-item")
+    .map(product => {
+      const result = { inStock: true };
+      result.itemId = product
+        .querySelector(".add-to-cart-btn a")
+        .getAttribute("data-product-id");
+      result.itemName = product
+        .querySelector("div.product-item__info > a")
+        .innerText.trim();
+      result.itemUrl = extractProductUrl(
+        product
+          .querySelector("div.product-item__info > a")
+          .getAttribute("onclick")
+      );
+      result.img = product.querySelector(".image-link img").getAttribute("src");
 
-Apify.main(async function main() {
+      result.currentPrice = parseFloat(
+        cleanPriceText(
+          product
+            .querySelector(".money-price > span:last-child")
+            .innerText.trim()
+        )
+      );
+      const originalPrice = product
+        .querySelector(".money-price__amount--original")
+        ?.innerText?.trim();
+      result.originalPrice = originalPrice
+        ? parseFloat(cleanPriceText(originalPrice))
+        : null;
+      result.currentUnitPrice = parseFloat(
+        cleanUnitPriceText(
+          product.querySelector(".product-item__sale-volume").innerText.trim()
+        )
+      );
+      result.useUnitPrice = product
+        .querySelector(".product-item__info")
+        .innerText.includes("cca");
+      result.discounted = result.currentPrice < result.originalPrice;
+      result.currency = "CZK";
+      result.category = category;
+      return result;
+    });
+}
+
+function extractProductUrl(onclickAttr) {
+  if (!onclickAttr) return null;
+  const regexp = /\'(\S+)\'/m;
+  const match = regexp.exec(onclickAttr);
+  return `${rootUrl}${match[1]}`;
+}
+
+async function main() {
   rollbar.init();
-  let stats = {};
-  const processedIds = new Set();
   const s3 = new S3Client({ region: "eu-central-1", maxAttempts: 3 });
   const cloudfront = new CloudFrontClient({
     region: "eu-central-1",
     maxAttempts: 3
   });
 
-  const input = await Apify.getInput();
+  const input = (await KeyValueStore.getInput()) ?? {};
   const {
-    development = false,
+    development = process.env.TEST,
     maxRequestRetries = 3,
     maxConcurrency = 25,
     proxyGroups = ["CZECH_LUMINATI"],
-    store = STORES.ZLI,
-    type = TYPE.FULL
-  } = input ?? {};
-  const requestQueue = await Apify.openRequestQueue();
+    store = Stores.ZLI,
+    type = ActorType.Full
+  } = input;
 
-  stats = (await Apify.getValue("STATS")) || {
-    categories: 0,
-    pages: 0,
-    items: 0,
-    itemsDuplicity: 0,
-    countItems: 0
-  };
+  const stats = await withPersistedStats(
+    x => x,
+    (await KeyValueStore.getValue("STATS")) || {
+      categories: 0,
+      pages: 0,
+      items: 0,
+      countItems: 0
+    }
+  );
 
   if (development) {
-    Apify.utils.log.setLevel(Apify.utils.log.LEVELS.DEBUG);
+    log.setLevel(LogLevel.DEBUG);
   }
 
-  const crawlContext = {
-    requestQueue,
-    development,
-    type,
-    stats,
-    processedIds,
-    s3
-  };
-
-  const proxyConfiguration = await Apify.createProxyConfiguration({
-    groups: proxyGroups
+  const proxyConfiguration = await Actor.createProxyConfiguration({
+    groups: proxyGroups,
+    useApifyProxy: !development
   });
-  if (type === TYPE.FULL) {
-    await requestQueue.addRequest({
-      url: rootUrl({ store }),
-      userData: { label: LABELS.START }
+
+  const crawler = new HttpCrawler({
+    proxyConfiguration,
+    maxRequestRetries,
+    maxConcurrency,
+    async requestHandler({ crawler, request, body }) {
+      const { document } = parseHTML(body.toString());
+      const { url, userData } = request;
+      const { label, category } = userData;
+      log.info("Page opened.", { label, category, url });
+      switch (label) {
+        case Labels.START:
+          if (type === ActorType.COUNT) {
+            const requests = document
+              .querySelectorAll(
+                "a.navigation-multilevel-node__link-inner--lvl-2"
+              )
+              .map(countLink => {
+                const url = new URL(countLink.getAttribute("href"), rootUrl);
+                if (!url.toString().includes("novinky")) {
+                  return {
+                    url: `${url.href}`,
+                    userData: {
+                      label: Labels.COUNT,
+                      category: countLink.innerText.trim()
+                    }
+                  };
+                }
+              });
+            await crawler.requestQueue.addRequests(requests);
+          } else {
+            const requests = document
+              .querySelectorAll(
+                ".menu > li.filter-category__item--level-2 > button.filter-category__link, " +
+                  ".menu > li.filter-category__item--level-2 > div > div > button.filter-category__link, " +
+                  ".menu > li.filter-category__item--level-3 > button.filter-category__link, " +
+                  ".menu > li.filter-category__item--level-3 > div > div > button.filter-category__link"
+              )
+              .map(link => {
+                const url = new URL(link.getAttribute("data-url"), rootUrl);
+                return {
+                  url: `${url.href}`,
+                  userData: {
+                    label: Labels.LIST,
+                    page: 0,
+                    category: link.innerText.trim()
+                  }
+                };
+              });
+            stats.add("categories", requests.length);
+            log.info(`Found ${requests.length}x categories`);
+            await crawler.requestQueue.addRequests(requests);
+          }
+          break;
+        case Labels.LIST:
+          stats.inc("pages");
+          if (userData.page === 0) {
+            const lastPageLink =
+              document
+                .querySelectorAll(
+                  ".pagination .pagination__step-cz:not(.pagination__step--next-cz)"
+                )
+                .at(-1)
+                ?.getAttribute("href") ?? "";
+
+            const paginationLink = new URL(lastPageLink, rootUrl);
+            const pagesTotal = paginationLink.searchParams.get("page");
+            const requests = [];
+            for (let i = 2; i <= Number(pagesTotal); i++) {
+              paginationLink.searchParams.set("page", i.toString());
+              userData.page = i;
+              requests.push({
+                url: paginationLink.href,
+                userData
+              });
+            }
+            await crawler.requestQueue.addRequests(requests, {
+              forefront: true
+            });
+          }
+
+          const products = extractItems(document, userData.category);
+          const requests = [Dataset.pushData(products)];
+          for (const product of products) {
+            requests.push(uploadToS3v2(s3, product));
+            stats.inc("items");
+          }
+          log.info(`Found ${products.length} products`);
+          await Promise.all(requests);
+          break;
+        case Labels.COUNT:
+          const count = document("span.category-number-of-products")
+            .innerText.trim()
+            .match(/\d+/)[0];
+          stats.add("countItems", Number(count));
+          log.info(
+            `Found ${count} items in category ${request.userData.category}`
+          );
+          break;
+        default:
+          log.error(`Unknown label ${label}`);
+      }
+    },
+    async failedRequestHandler({ request, log }, error) {
+      log.error(`Request ${request.url} failed multiple times`, error);
+    }
+  });
+
+  const startingRequests = [];
+  if (type === ActorType.Full) {
+    startingRequests.push({
+      url: `${rootUrl}/store/switch?store=${store}&referer-url=/cs/outlet`,
+      userData: { label: Labels.START }
     });
-  } else if (type === TYPE.TEST) {
-    await requestQueue.addRequest({
+  } else if (type === ActorType.Test) {
+    startingRequests.push({
       url: `https://shop.iglobus.cz/cs/sv%C4%9Bt-d%C4%9Bt%C3%AD/d%C4%9Btsk%C3%A1-v%C3%BD%C5%BEiva/p%C5%99%C3%ADkrmy/ovocn%C3%A9`,
       userData: {
-        label: LABELS.LIST,
+        label: Labels.LIST,
         page: 0,
         category: "Ovocné"
       }
     });
   }
-  const crawler = new Apify.CheerioCrawler({
-    requestQueue,
-    proxyConfiguration,
-    maxRequestRetries,
-    maxConcurrency: development ? 1 : maxConcurrency,
-
-    handlePageFunction: async context => {
-      const {
-        url,
-        userData: { label, category }
-      } = context.request;
-      log.info("Page opened.", { label, category, url });
-      switch (label) {
-        case LABELS.START:
-          return handleStart(context, crawlContext);
-        case LABELS.LIST:
-          return handleList(context, crawlContext);
-        case LABELS.COUNT:
-          return handleCount(context, crawlContext);
-        default:
-          log.error(`Unknown label ${label}`);
-      }
-    }
-  });
-
   log.info("Starting the crawl.");
-  await crawler.run();
+  await crawler.run(startingRequests);
   log.info("Crawl finished.");
 
-  await Apify.setValue("STATS", stats);
+  await KeyValueStore.setValue("STATS", stats);
   log.debug("STATS saved!");
-  log.info(JSON.stringify(stats));
 
   if (!development) {
     await invalidateCDN(cloudfront, "EQYSHWUECAQC9", "iglobus.cz");
@@ -133,158 +257,6 @@ Apify.main(async function main() {
     log.info("upload to Keboola finished");
   }
   log.info("Finished.");
-});
-
-async function handleStart({ request, $ }, crawlContext) {
-  const { requestQueue, stats } = crawlContext;
-  const listLinks = [
-    ".menu > li.filter-category__item--level-2 > button.filter-category__link",
-    ".menu > li.filter-category__item--level-2 > div > div > button.filter-category__link",
-    ".menu > li.filter-category__item--level-3 > button.filter-category__link",
-    ".menu > li.filter-category__item--level-3 > div > div > button.filter-category__link"
-  ]
-    .map(selector => $(selector).get())
-    .flat();
-  const countLinks = ["a.navigation-multilevel-node__link-inner--lvl-2"]
-    .map(selector => $(selector).get())
-    .flat();
-  if (crawlContext.type === TYPE.COUNT) {
-    for (const countLink of countLinks) {
-      const url = new URL($(countLink).attr("href"), ROOT_URL);
-      if (!url.toString().includes("novinky")) {
-        await requestQueue.addRequest({
-          url: `${url.href}`,
-          userData: {
-            label: LABELS.COUNT,
-            category: $(countLink).text().trim()
-          }
-        });
-      }
-    }
-  } else {
-    for (const link of listLinks) {
-      const url = new URL($(link).attr("data-url"), ROOT_URL);
-      await requestQueue.addRequest({
-        url: `${url.href}`,
-        userData: {
-          label: LABELS.LIST,
-          page: 0,
-          category: $(link).text().trim()
-        }
-      });
-    }
-    stats.categories = listLinks.length;
-    log.info(`Found ${listLinks.length}x categories`);
-  }
 }
 
-// create function handleCount copied from handleList
-async function handleCount({ request, $ }, crawlContext) {
-  const { requestQueue, stats } = crawlContext;
-  const count = $("span.category-number-of-products")
-    .text()
-    .trim()
-    .match(/\d+/)[0];
-  stats.countItems += parseInt(count);
-  log.info(`Found ${count} items in category ${request.userData.category}`);
-}
-
-async function handleList({ request, $ }, crawlContext) {
-  // Handle pagination
-  const { url, userData } = request;
-  const { requestQueue, processedIds, stats, s3 } = crawlContext;
-  stats.pages++;
-  if (userData.page === 0) {
-    const lastPageLink = $(
-      ".pagination .pagination__step-cz:not(.pagination__step--next-cz)"
-    )
-      .last()
-      .attr("href");
-
-    const paginationLink = new URL(lastPageLink, ROOT_URL);
-
-    // get total pagination pages in current category
-    const pagesTotal = paginationLink.searchParams.get("page");
-
-    //get last pagination page
-    for (let i = 2; i <= pagesTotal; i++) {
-      paginationLink.searchParams.set("page", i.toString());
-      userData.page = i;
-      await requestQueue.addRequest(
-        {
-          url: paginationLink.href,
-          userData
-        },
-        { forefront: true }
-      );
-    }
-  }
-  // Handle products on list
-  const $products = $(".product-list product-item");
-  try {
-    const products = await extractItems($, $products, userData.category);
-    // we don't need to block pushes, we will await them all at the end
-    const requests = [];
-    for (const product of products) {
-      if (!processedIds.has(product.itemId)) {
-        processedIds.add(product.itemId);
-        requests.push(Apify.pushData(product), uploadToS3v2(s3, product));
-        stats.items++;
-      } else {
-        stats.itemsDuplicity++;
-      }
-    }
-    log.info(`Found ${requests.length / 2} unique products`);
-    // await all requests, so we don't end before they end
-    await Promise.all(requests);
-  } catch (e) {
-    console.error(e);
-    console.log(`Failed extraction of items. ${request.url}`);
-  }
-}
-
-async function extractItems($, $products, category) {
-  const itemsArray = [];
-  $products.each(function () {
-    const result = { inStock: true };
-    result.itemId = $(this).find(".add-to-cart-btn a").attr("data-product-id");
-    result.itemName = $(this).find("div.product-item__info > a").text().trim();
-    result.itemUrl = extractProductUrl(
-      $(this).find("div.product-item__info > a").attr("onclick")
-    );
-    result.img = $(this).find(".image-link img").attr("src");
-
-    result.currentPrice = parseFloat(
-      cleanPriceText(
-        $(this).find(".money-price > span:last-child").text().trim()
-      )
-    );
-    result.originalPrice = parseFloat(
-      cleanPriceText(
-        $(this).find(".money-price__amount--original").text().trim()
-      )
-    );
-    result.currentUnitPrice = parseFloat(
-      cleanUnitPriceText(
-        $(this).find(".product-item__sale-volume").text().trim()
-      )
-    );
-    result.useUnitPrice = $(this)
-      .find(".product-item__info")
-      .text()
-      .includes("cca");
-    result.discounted = result.currentPrice < result.originalPrice;
-    result.currency = "CZK";
-    // result.instStock =
-    result.category = category;
-    itemsArray.push(result);
-  });
-  return itemsArray;
-}
-
-function extractProductUrl(onclickAttr) {
-  if (!onclickAttr) return null;
-  const regexp = /\'(\S+)\'/m;
-  const match = regexp.exec(onclickAttr);
-  return `${ROOT_URL}${match[1]}`;
-}
+await Actor.main(main);
