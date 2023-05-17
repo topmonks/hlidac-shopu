da949f89608c90b1f1f2c5b9994d53ea210a0fb1
Jakub Dušek <jakub.dusek@salax.cz>
2023-05-17 10:02:32 +0200

    [actors/allegro-daily] fix import

--- actors/allegro-daily/main.js
+++ actors/allegro-daily/main.js
@@ -1,4 +1,4 @@
-import { getInput } from "../common/crawler.js";
+import { getInput } from "@hlidac-shopu/actors-common/crawler.js";
 import { cleanPrice } from "@hlidac-shopu/actors-common/product.js";
 import Rollbar from "@hlidac-shopu/actors-common/rollbar.js";
 import { withPersistedStats } from "@hlidac-shopu/actors-common/stats.js";
