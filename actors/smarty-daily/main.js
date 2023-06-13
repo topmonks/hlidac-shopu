e91c8dfa699762807acd67ad98921c8f788f1734
Pavel 'Strajk' Dolecek <strajk@me.com>
2023-06-13 15:31:31 +0200

    CR

--- actors/smarty-daily/main.js
+++ actors/smarty-daily/main.js
@@ -1,55 +1,57 @@
 import { Actor } from "apify";
 import { LinkeDOMCrawler, createLinkeDOMRouter } from "@crawlee/linkedom";
-import { gotScraping } from "got-scraping";
-import { DOMParser } from "linkedom/cached";
 import rollbar from "@hlidac-shopu/actors-common/rollbar.js";
 import { uploadToKeboola } from "@hlidac-shopu/actors-common/keboola.js";
 import { cleanPriceText } from "@hlidac-shopu/lib/parse.mjs";
 
-const LABEL = {
-  INDEX: "INDEX",
-  LISTING: "LISTING",
-  DETAIL: "DETAIL"
-};
+const LABEL_CUSTOM = {
+  INDEX: `INDEX`,
+  LISTING_PREFLIGHT: `LISTING_PREFLIGHT`,
+  LISTING: `LISTING`,
+}
 
 const baseUrl = "https://www.smarty.cz";
 
 async function crawl() {
   rollbar.init();
-  const domParser = new DOMParser();
   const crawler = new LinkeDOMCrawler({
     requestHandler: createLinkeDOMRouter({
-      [LABEL.INDEX]: async ({ window: { document }, crawler }) => {
+      [LABEL_CUSTOM.INDEX]: async ({ window: { document }, crawler }) => {
         const urls = Array.from(document.querySelectorAll(`url loc`)).map((el) => el.innerText);
         // https://www.smarty.cz/Vyrobce/samsung - we wanna just these
         // https://www.smarty.cz/Vyrobce/samsung/mobilni-telefony - but not these deep urls
         const brandsUrls = urls.filter(url => url.split(`/`).length === 5);
         const { addedRequests } = await crawler.addRequests(brandsUrls.map(url => ({
           url: `${url}#us=1&s=n&pg=all`,
-          label: LABEL.LISTING
+          label: LABEL_CUSTOM.LISTING_PREFLIGHT
         })));
       },
-      [LABEL.LISTING]: async ({ window: { document }, request, log }) => {
+      [LABEL_CUSTOM.LISTING_PREFLIGHT]: async ({ window: { document }, crawler }) => {
         const producerId = document.querySelector(`#fId`).value;
         const producerAlias = document.querySelector(`#producerAlias`).value;
-        const res = await gotScraping({
-          url: `https://www.smarty.cz/Product/ProducerProductItems?${(new URLSearchParams({
-            producerId,
-            producerAlias,
-            itemTypeId: ``,
-            productFilter: `1`,
-            paramJson: JSON.stringify({ us: `1`, s: `n`, pg: `all` }),
-            sort: `n`,
-            page: `all`
-          }))}`
+        const url = `https://www.smarty.cz/Product/ProducerProductItems?${(new URLSearchParams({
+          producerId,
+          producerAlias,
+          itemTypeId: ``,
+          productFilter: `1`,
+          paramJson: JSON.stringify({ us: `1`, s: `n`, pg: `all` }),
+          sort: `n`,
+          page: `all`
+        }))}`;
+        await crawler.addRequests([{
+          url,
+          label: LABEL_CUSTOM.LISTING
+        }], {
+          forefront: true // makes debugging easier, cause it reveals problems in LISTING handler faster
         });
-        const doc = domParser.parseFromString(res.body, `text/html`);
-        if (doc.querySelector(`.errorPage`)) {
-          log.error(`errorPage`, { body: res.body });
+      },
+      [LABEL_CUSTOM.LISTING]: async ({ window: { document }, log }) => {
+        if (document.querySelector(`.errorPage`)) {
+          log.error(`errorPage`, { html: document.innerHTML });
           return;
         }
         const products = [];
-        doc.querySelectorAll(`.item`).forEach((el) => {
+        document.querySelectorAll(`.item`).forEach((el) => {
           const productJson = JSON.parse(el.getAttribute(`data-gaItem`));
           const urlRel = el.getAttribute(`data-url`);
           const product = {
@@ -68,7 +70,7 @@ async function crawl() {
       }
     })
   });
-  await crawler.run([{ url: `${baseUrl}/Feed/Sitemap/Producers`, label: LABEL.INDEX }]);
+  await crawler.run([{ url: `${baseUrl}/Feed/Sitemap/Producers`, label: LABEL_CUSTOM.INDEX }]);
   // await crawler.run([{ url: `${baseUrl}/Vyrobce/samsung#us=1&s=n&pg=all`, label: LABEL.LISTING }]) // for testing
   await uploadToKeboola("smarty_cz");
 }
