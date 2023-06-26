24ea72f02a5e78019f72e28d29a3c198d73d5e44
codefactor-io <support@codefactor.io>
2023-06-26 07:40:15 +0000

    [CodeFactor] Apply fixes

--- actors/smarty-daily/main.js
+++ actors/smarty-daily/main.js
@@ -11,8 +11,8 @@ import { withPersistedStats } from "@hlidac-shopu/actors-common/stats.js";
 const LABEL_CUSTOM = {
   INDEX: `INDEX`,
   LISTING_PREFLIGHT: `LISTING_PREFLIGHT`,
-  LISTING: `LISTING`,
-}
+  LISTING: `LISTING`
+};
 
 const baseUrl = "https://www.smarty.cz";
 
@@ -23,7 +23,7 @@ async function main() {
     development,
     type = ActorType.Full,
     proxyGroups,
-    maxRequestRetries,
+    maxRequestRetries
   } = await getInput();
 
   const stats = await withPersistedStats(x => x, {
@@ -42,70 +42,104 @@ async function main() {
     maxRequestRetries,
     requestHandler: createLinkeDOMRouter({
       [LABEL_CUSTOM.INDEX]: async ({ window: { document }, crawler }) => {
-        const urls = Array.from(document.querySelectorAll(`url loc`)).map((el) => el.innerText);
+        const urls = Array.from(document.querySelectorAll(`url loc`)).map(
+          el => el.innerText
+        );
         // https://www.smarty.cz/Vyrobce/samsung - we wanna just these
         // https://www.smarty.cz/Vyrobce/samsung/mobilni-telefony - but not these deep urls
         const brandsUrls = urls.filter(url => url.split(`/`).length === 5);
-        const { addedRequests } = await crawler.addRequests(brandsUrls.map(url => ({
-          url: `${url}#us=1&s=n&pg=all`,
-          label: LABEL_CUSTOM.LISTING_PREFLIGHT
-        })));
+        const { addedRequests } = await crawler.addRequests(
+          brandsUrls.map(url => ({
+            url: `${url}#us=1&s=n&pg=all`,
+            label: LABEL_CUSTOM.LISTING_PREFLIGHT
+          }))
+        );
         stats.add(`categories`, brandsUrls.length);
       },
-      [LABEL_CUSTOM.LISTING_PREFLIGHT]: async ({ window: { document }, crawler }) => {
+      [LABEL_CUSTOM.LISTING_PREFLIGHT]: async ({
+        window: { document },
+        crawler
+      }) => {
         const producerId = document.querySelector(`#fId`).value;
         const producerAlias = document.querySelector(`#producerAlias`).value;
-        const url = `https://www.smarty.cz/Product/ProducerProductItems?${(new URLSearchParams({
-          producerId,
-          producerAlias,
-          itemTypeId: ``,
-          productFilter: `1`,
-          paramJson: JSON.stringify({ us: `1`, s: `n`, pg: `all` }),
-          sort: `n`,
-          page: `all`
-        }))}`;
-        await crawler.addRequests([{
-          url,
-          label: LABEL_CUSTOM.LISTING
-        }], {
-          forefront: true // makes debugging easier, cause it reveals problems in LISTING handler faster
-        });
+        const url = `https://www.smarty.cz/Product/ProducerProductItems?${new URLSearchParams(
+          {
+            producerId,
+            producerAlias,
+            itemTypeId: ``,
+            productFilter: `1`,
+            paramJson: JSON.stringify({ us: `1`, s: `n`, pg: `all` }),
+            sort: `n`,
+            page: `all`
+          }
+        )}`;
+        await crawler.addRequests(
+          [
+            {
+              url,
+              label: LABEL_CUSTOM.LISTING
+            }
+          ],
+          {
+            forefront: true // makes debugging easier, cause it reveals problems in LISTING handler faster
+          }
+        );
       },
       [LABEL_CUSTOM.LISTING]: async ({ window: { document }, log }) => {
         if (document.querySelector(`.errorPage`)) {
           log.error(`errorPage`, { html: document.innerHTML });
           return;
         }
-        const products = Array.from(document.querySelectorAll(`.item`)).map((el) => {
-          const productJson = JSON.parse(el.getAttribute(`data-gaItem`));
-          const urlRel = el.getAttribute(`data-url`);
-          return {
-            itemId: el.querySelector(`.icon.compare`).getAttribute(`data-id`),
-            itemUrl: `${baseUrl}${urlRel}`,
-            itemName: productJson.name,
-            currentPrice: productJson.fullPrice,
-            originalPrice: el.querySelector(`.buy .price .d .o`) && parseFloat(cleanPriceText(el.querySelector(`.buy .price .d .o`)?.textContent)),
-            currency: `CZK`,
-            img: el.querySelector(`picture img`)?.getAttribute(`src`),
-            inStock: productJson.available.includes(`Skladem`)
-          };
-        });
+        const products = Array.from(document.querySelectorAll(`.item`)).map(
+          el => {
+            const productJson = JSON.parse(el.getAttribute(`data-gaItem`));
+            const urlRel = el.getAttribute(`data-url`);
+            return {
+              itemId: el.querySelector(`.icon.compare`).getAttribute(`data-id`),
+              itemUrl: `${baseUrl}${urlRel}`,
+              itemName: productJson.name,
+              currentPrice: productJson.fullPrice,
+              originalPrice:
+                el.querySelector(`.buy .price .d .o`) &&
+                parseFloat(
+                  cleanPriceText(
+                    el.querySelector(`.buy .price .d .o`)?.textContent
+                  )
+                ),
+              currency: `CZK`,
+              img: el.querySelector(`picture img`)?.getAttribute(`src`),
+              inStock: productJson.available.includes(`Skladem`)
+            };
+          }
+        );
         stats.add(`items`, products.length);
         await Actor.pushData(products);
       }
     }),
     failedRequestHandler({ request, log }, error) {
-      log.error(`Request ${request.url} failed ${request.retryCount} times`, error);
+      log.error(
+        `Request ${request.url} failed ${request.retryCount} times`,
+        error
+      );
       stats.inc("failed");
     }
   });
 
   if (type === ActorType.Full) {
-    await crawler.run([{ url: `${baseUrl}/Feed/Sitemap/Producers`, label: LABEL_CUSTOM.INDEX }]);
+    await crawler.run([
+      { url: `${baseUrl}/Feed/Sitemap/Producers`, label: LABEL_CUSTOM.INDEX }
+    ]);
   } else if (type === ActorType.Test) {
-    await crawler.run([{ url: `${baseUrl}/Vyrobce/samsung#us=1&s=n&pg=all`, label: LABEL.LISTING }]);
+    await crawler.run([
+      {
+        url: `${baseUrl}/Vyrobce/samsung#us=1&s=n&pg=all`,
+        label: LABEL.LISTING
+      }
+    ]);
   } else {
-    throw new Error(`Unknown actor type ${type}, supported types are ${ActorType.Full} and ${ActorType.Test}`);
+    throw new Error(
+      `Unknown actor type ${type}, supported types are ${ActorType.Full} and ${ActorType.Test}`
+    );
   }
 
   if (!development) {
