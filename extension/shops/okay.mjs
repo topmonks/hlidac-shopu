62e3c73365898bbc18e123112f37d603bd0166d5
Aleš Roubíček <ales@roubicek.name>
2023-10-25 20:27:42 +0200

    Fixed Okay extension

--- extension/shops/okay.mjs
+++ extension/shops/okay.mjs
@@ -2,45 +2,25 @@ import { cleanPrice, registerShop, isElementVisible } from "../helpers.mjs";
 import { Shop } from "./shop.mjs";
 
 export class Okay extends Shop {
+  selector = ".product-form-container";
+
   get injectionPoint() {
-    return ["afterend", ".product-form-container"];
+    return ["afterend", this.selector, { display: "block !important" }];
   }
 
   async scrape() {
-    const elem = document.querySelector("#template-product");
+    const elem = document.querySelector(this.selector);
     if (!elem) return;
-    const itemId = document.querySelector("div.product-gallery__main")
-      .attributes["data-product-id"].textContent;
-    const manufacturersRecommendedPriceSel = ".modal_price .was-price .money";
-    const manufacturersRecommendedPriceVisible = isElementVisible(
-      document.querySelector(manufacturersRecommendedPriceSel)
+    const product = JSON.parse(
+      elem.querySelector("[data-product]").dataset.product
     );
-    const manufacturersRecommendedPrice = manufacturersRecommendedPriceVisible
-      ? cleanPrice(manufacturersRecommendedPriceSel)
-      : null;
-
-    const priceSel = ".modal_price .current_price .money";
-    const priceVisible = isElementVisible(document.querySelector(priceSel));
-    const price = priceVisible ? cleanPrice(priceSel) : null;
-
-    const priceAfterDiscountSel = ".modal_price  .current_price_mz .money.sale";
-    const priceAfterDiscountVisible = isElementVisible(
-      document.querySelector(priceAfterDiscountSel)
-    );
-    const priceAfterDiscount = priceAfterDiscountVisible
-      ? cleanPrice(priceAfterDiscountSel)
-      : null;
-
-    const originalPrice = manufacturersRecommendedPrice
-      ? manufacturersRecommendedPrice
-      : priceAfterDiscount
-      ? price
-      : null;
-    console.log("originalPrice", originalPrice);
-    const currentPrice = priceAfterDiscount ? priceAfterDiscount : price;
-    console.log("currentPrice", currentPrice);
-    const title = elem.querySelector("h1").innerText.trim();
-    const imageUrl = document.querySelector(".product-gallery__link").href;
+    const itemId = product.id;
+    const comparePrice = product.compare_at_price / 100;
+    const currentPrice = product.price / 100;
+    const originalPrice =
+      product.compare_at_price !== product.price ? comparePrice : null;
+    const title = product.title;
+    const imageUrl = product.featured_image;
     return { itemId, title, currentPrice, originalPrice, imageUrl };
   }
 }
