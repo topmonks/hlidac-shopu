cec692259ada3e8d9bb92e25803086d3cd5495c1
Aleš Roubíček <ales@roubicek.name>
2023-10-25 20:09:44 +0200

    Fixed Mironet extension

--- extension/shops/mironet.mjs
+++ extension/shops/mironet.mjs
@@ -1,19 +1,18 @@
-import { cleanPrice, registerShop } from "../helpers.mjs";
+import { cleanPrice, cleanPriceText, registerShop } from "../helpers.mjs";
 import { Shop } from "./shop.mjs";
 
 export class Mironet extends Shop {
+  selector = ".product_btn_kosik";
   get injectionPoint() {
-    return ["afterend", ".product_kosik_info"];
+    return ["afterend", this.selector];
   }
 
   async scrape() {
-    const elem = document.querySelector(".product_detail");
+    const elem = document.querySelector(this.selector);
     if (!elem) return;
-    const itemId = document.querySelector(
-      ".product_kosik_info input[name=Code]"
-    ).value;
-    const title = document.querySelector("h1").textContent.trim();
-    const currentPrice = cleanPrice(".product_cena_box .product_dph");
+    const itemId = elem.querySelector("input[name=Code]").value;
+    const title = elem.querySelector("input[name=NameItem]").value;
+    const currentPrice = cleanPriceText(elem.querySelector("input[name=Price]").value);
     const originalPrice = cleanPrice(".fakcbox23 .product_dph span");
     const imageUrl = document.getElementById("DetailImg").src;
 
