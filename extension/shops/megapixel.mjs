8c441b90e499af6e5cc0cde8e8c6e8149f72ebe7
Pavel 'Strajk' Dolecek <strajk@me.com>
2024-10-11 20:13:12 +0200

    Apply /Users/strajk/Downloads/__TRIGGER/_megapixel

--- extension/shops/megapixel.mjs
+++ extension/shops/megapixel.mjs
@@ -9,7 +9,9 @@ export class Megapixel extends Shop {
   async scrape() {
     const elem = document.querySelector("div#snippet--price");
     if (!elem) return;
-    const itemId = document.querySelector("[data-product-item-id]").getAttribute("data-product-item-id");
+    let itemId = document.querySelector("[data-product-item-id]")?.getAttribute("data-product-item-id");
+    // Some products do not have [data-product-item-id], e.g. https://www.megapixel.cz/peak-design-micro-clutch-i-plate
+    itemId ??= document.querySelector(".product-detail a[href^='/hlidaci-pes-produktu?item=']")?.href.split("=")[1];
     const title = document.querySelector("h1").innerText.trim();
     const currentPrice = cleanPrice(".product-detail__price-vat");
     const originalPrice = cleanPrice(".product-detail__price del");
