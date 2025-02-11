4d96b8a220edfee3c9325e6560d45d7660637480
Aleš Roubíček <ales@roubicek.name>
2025-02-11 10:54:32 +0100

    Fix #2786

--- extension/shops/albert.mjs
+++ extension/shops/albert.mjs
@@ -1,23 +1,29 @@
 import { registerShop } from "../helpers.mjs";
-import { AsyncShop } from "./shop.mjs";
+import { StatefulShop } from "./shop.mjs";
 
-export class Albert extends AsyncShop {
-  selector = `main>script[type='application/ld+json']`;
+export class Albert extends StatefulShop {
+  get detailSelector() {
+    return ":has([data-testid=product-block-price])";
+  }
+
+  shouldRender(mutations) {
+    return Boolean(mutations.find(x => x.target.dataset.testid === "product-carousel"));
+  }
+
+  shouldCleanup(mutations) {
+    return Boolean(mutations.find(x => Array.from(x.removedNodes).find(y => y.id === "product-details-seo-data")));
+  }
 
   get injectionPoint() {
     return ["afterend", `[data-testid=product-properties]`];
   }
 
-  get waitForSelector() {
-    return "[data-testid=price-wrapper]";
-  }
-
   async scrape() {
-    const data = JSON.parse(document.querySelector(this.selector).textContent);
+    const data = JSON.parse(document.querySelector("#product-details-seo-data").textContent);
     if (!data) return;
     const itemId = data.url.split("/").at(-1);
     const title = data.name;
-    const currentPrice = toCZK(document.querySelector("[data-testid=price-wrapper]").textContent);
+    const currentPrice = toCZK(document.querySelector("[data-testid=product-block-price]").textContent);
     const originalPrice = data.offers.priceSpecification.price;
     const imageUrl = data.image[0];
     return { itemId, title, currentPrice, originalPrice, imageUrl };
