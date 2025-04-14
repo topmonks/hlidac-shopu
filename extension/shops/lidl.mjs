937d28daeb41aa4ddea8a51e2b4bcdf50b4cd92f
Juraj Hrenák <judzinator@gmail.com>
2025-04-14 21:02:40 +0200

    fix: lidl.cz rendering

--- extension/shops/lidl.mjs
+++ extension/shops/lidl.mjs
@@ -3,21 +3,21 @@ import { AsyncShop } from "./shop.mjs";
 
 export class Lidl extends AsyncShop {
   get waitForSelector() {
-    return ".buybox:has(.delivery-info)";
+    return ".detail-one .block--mobile-hidden .buybox-one";
   }
 
   get injectionPoint() {
-    return ["beforebegin", ".buybox__item:has(.delivery-info)"];
+    return ["beforebegin", ".detail-one .block--mobile-hidden .buybox-one"];
   }
 
   async scrape() {
-    const elem = document.querySelector(".buybox");
+    const elem = document.querySelector(".block--mobile-hidden .heading__title");
     if (!elem) return;
-    const itemId = document.querySelector(".buybox__erp-number").textContent;
-    const title = document.querySelector(".keyfacts__title").textContent.trim();
-    const currentPrice = cleanPrice(".m-price__price");
-    const originalPrice = cleanPrice(".m-price__rrp");
-    const imageUrl = document.querySelector(".gallery-image__img").src;
+    const itemId = document.querySelector(".block--mobile-hidden .product-id__number").textContent;
+    const title = document.querySelector(".block--mobile-hidden [data-qa-label='keyfacts-title']").textContent.trim();
+    const currentPrice = cleanPrice(".block--mobile-hidden .buybox-one .m-price__price");
+    const originalPrice = cleanPrice(".block--mobile-hidden .buybox-one .m-price__rrp");
+    const imageUrl = document.querySelector(".media-carousel-slider .media-carousel-item[data-index='0'] .image").src;
 
     return { itemId, title, currentPrice, originalPrice, imageUrl };
   }
