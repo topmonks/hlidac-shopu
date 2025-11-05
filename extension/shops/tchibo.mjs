4f36003ecc5c997cafbba9c666afbeeb6797ceaa
Václav Slavík <vaclav.slavik@topmonks.com>
2025-11-05 09:04:26 +0100

    [extension] #3196 - fix extension rendering and originalPrice extraction

--- extension/shops/tchibo.mjs
+++ extension/shops/tchibo.mjs
@@ -2,7 +2,7 @@ import { cleanPrice, registerShop } from "../helpers.mjs";
 import { AsyncShop } from "./shop.mjs";
 
 export class Tchibo extends AsyncShop {
-  selector = ".crosschannel-panel";
+  selector = ".crosschannel";
 
   get injectionPoint() {
     return ["afterend", this.selector];
@@ -13,13 +13,14 @@ export class Tchibo extends AsyncShop {
   }
 
   async scrape() {
+
     const elem = document.querySelector(".pdp-buybox__add-to-cart-container");
     if (!elem) return;
     const itemUrl = document.location.href;
     const itemId = itemUrl.split("/").at(-2);
     const title = document.querySelector(".pdp-buybox__title").textContent.trim();
     const currentPrice = cleanPrice("#pdp-buybox .tp-price-current .tp-price-number");
-    const originalPrice = cleanPrice("#pdp-buybox .tp-price-previous .tp-price-number");
+    const originalPrice = cleanPrice("#pdp-buybox .pdp-price-display__lowest-price-note .tp-price-number");
     const imageUrl = document.querySelector(".tp-imagegallery-main-container img")?.src;
 
     return { itemId, title, currentPrice, originalPrice, imageUrl };
